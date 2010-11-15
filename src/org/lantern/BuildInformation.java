4574e04e3353cde7176b29231f119d05c857f3b8
Martin Berglund <mabe02@gmail.com>
2010-11-15 22:19:27 +0900

    Tagging 1.0 release candidate 1

--- src/org/lantern/BuildInformation.java
+++ src/org/lantern/BuildInformation.java
@@ -2,7 +2,7 @@
 package org.lantern;
 public class BuildInformation
 {
-    public static final String VERSION = "1.0-beta3";
-    public static final int BUILD = 16;
+    public static final String VERSION = "1.0-rc1";
+    public static final int BUILD = 19;
 }
         
