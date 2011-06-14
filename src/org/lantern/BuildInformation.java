9e917b30994e39804fc99105c1c549c44b920c40
Martin Berglund <mabe02@gmail.com>
2011-06-14 11:24:24 +0900

    Release candidate 2, build 45

--- src/org/lantern/BuildInformation.java
+++ src/org/lantern/BuildInformation.java
@@ -2,7 +2,7 @@
 package org.lantern;
 public class BuildInformation
 {
-    public static final String VERSION = "1.0-rc1";
-    public static final int BUILD = 43;
+    public static final String VERSION = "1.0-rc2";
+    public static final int BUILD = 45;
 }
         
