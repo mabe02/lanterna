f9170e46e2b82351b7b47aa1c37a77c06daaf8a3
Martin Berglund <mabe02@gmail.com>
2011-10-31 22:35:15 +0900

    Unused import

--- src/org/lantern/screen/Screen.java
+++ src/org/lantern/screen/Screen.java
@@ -24,8 +24,6 @@ import java.util.EnumSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.lantern.LanternException;
 import org.lantern.input.InputProvider;
