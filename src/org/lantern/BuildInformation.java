c00b737337981b0fd92e0f57d79bef1a2209bf93
Martin Berglund <mabe02@gmail.com>
2011-10-31 22:03:10 +0900

    Tagging 1.0.1

--- src/org/lantern/BuildInformation.java
+++ src/org/lantern/BuildInformation.java
@@ -2,8 +2,8 @@ package org.lantern;
 
 public class BuildInformation
 {
-    public static final String VERSION = "1.0";
-    public static final int BUILD = 46;
+    public static final String VERSION = "1.0.1";
+    public static final int BUILD = 47;
 }
 
         
