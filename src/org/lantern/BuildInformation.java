dce0f126d66296e6854ebf4e24115da42275f56f
Martin Berglund <mabe02@gmail.com>
2011-10-31 22:04:33 +0900

    Beginning 1.0.2

--- src/org/lantern/BuildInformation.java
+++ src/org/lantern/BuildInformation.java
@@ -2,8 +2,8 @@ package org.lantern;
 
 public class BuildInformation
 {
-    public static final String VERSION = "1.0.1";
-    public static final int BUILD = 47;
+    public static final String VERSION = "1.0.2-SNAPSHOT";
+    public static final int BUILD = 48;
 }
 
         
