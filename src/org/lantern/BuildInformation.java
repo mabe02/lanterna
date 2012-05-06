3f9c1d2538612ed7d408dfcd1ed9e6d2279d918b
Martin Berglund <mabe02@gmail.com>
2012-05-06 13:52:14 +0900

    Lanterna 1.0.3

--- src/org/lantern/BuildInformation.java
+++ src/org/lantern/BuildInformation.java
@@ -2,8 +2,8 @@ package org.lantern;
 
 public class BuildInformation
 {
-    public static final String VERSION = "1.0.2";
-    public static final int BUILD = 51;
+    public static final String VERSION = "1.0.3";
+    public static final int BUILD = 52;
 }
 
         
