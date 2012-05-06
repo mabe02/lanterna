15f7a9c14d351327db4fc09e5b63a72eb4475ca6
Martin Berglund <mabe02@gmail.com>
2012-05-06 11:44:32 +0900

    This file is no longer needed, Maven will put a properties file for us

--- src/main/java/com/googlecode/lanterna/BuildInformation.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.lanterna;
-
-public class BuildInformation
-{
-    public static final String VERSION = "1.1-SNAPSHOT";
-    public static final int BUILD = 49;
-}
-
-        
