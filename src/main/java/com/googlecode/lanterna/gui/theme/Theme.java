d1f0ddae75ba9e98a5989e6a29ca62206cc82129
Martin Berglund <mabe02@gmail.com>
2012-05-06 04:01:34 +0100

    Updating license header

--- src/main/java/com/googlecode/lanterna/gui/theme/Theme.java
+++ src/main/java/com/googlecode/lanterna/gui/theme/Theme.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * Copyright (C) 2010-2011 mabe02
+ * Copyright (C) 2010-2012 mabe02
  */
 
 package com.googlecode.lanterna.gui.theme;
