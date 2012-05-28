577dbb857eb47aabbf6ba3744da18009f5f3bee7
Martin Berglund <mabe02@gmail.com>
2012-05-28 21:06:40 +0900

    Wrong variable name

--- src/main/java/com/googlecode/lanterna/input/PuttyProfile.java
+++ src/main/java/com/googlecode/lanterna/input/PuttyProfile.java
@@ -31,9 +31,9 @@ public class PuttyProfile extends CommonProfile
     @Override
     Collection<CharacterPattern> getPatterns()
     {
-        Collection<CharacterPattern> xtermPatterns = super.getPatterns();
-        xtermPatterns.add(new BasicCharacterPattern(new Key(Key.Kind.Home), ESC_CODE, '[', '1', '~'));
-        xtermPatterns.add(new BasicCharacterPattern(new Key(Key.Kind.End), ESC_CODE, '[', '4', '~'));
-        return xtermPatterns;
+        Collection<CharacterPattern> puttyPatterns = super.getPatterns();
+        puttyPatterns.add(new BasicCharacterPattern(new Key(Key.Kind.Home), ESC_CODE, '[', '1', '~'));
+        puttyPatterns.add(new BasicCharacterPattern(new Key(Key.Kind.End), ESC_CODE, '[', '4', '~'));
+        return puttyPatterns;
     }
 }
