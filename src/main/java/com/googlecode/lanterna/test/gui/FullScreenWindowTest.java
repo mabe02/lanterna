05db0dc92b73efa78fd2a39cb9667c6d018d5da5
Martin Berglund <mabe02@gmail.com>
2012-07-14 16:30:23 +0900

    Adding fullscreen support for windows

---
+++ src/main/java/com/googlecode/lanterna/test/gui/FullScreenWindowTest.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of lanterna (http://code.google.com/p/lanterna/).
+ * 
+ * lanterna is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright (C) 2010-2012 Martin
+ */
+
+package com.googlecode.lanterna.test.gui;
+
+import com.googlecode.lanterna.gui.Action;
+import com.googlecode.lanterna.gui.Border;
+import com.googlecode.lanterna.gui.GUIScreen;
+import com.googlecode.lanterna.gui.Window;
+import com.googlecode.lanterna.gui.component.Button;
+import com.googlecode.lanterna.gui.component.EmptySpace;
+import com.googlecode.lanterna.gui.component.Label;
+import com.googlecode.lanterna.gui.component.Panel;
+import com.googlecode.lanterna.gui.layout.SizePolicy;
+import com.googlecode.lanterna.test.TestTerminalFactory;
+
+/**
+ *
+ * @author Martin
+ */
+public class FullScreenWindowTest {
+   public static void main(String[] args)
+    {
+        final GUIScreen guiScreen = new TestTerminalFactory(args).createGUIScreen();
+        guiScreen.getScreen().startScreen();
+        final Window window1 = new Window("Full screen window");
+        window1.setBorder(new Border.Invisible());
+
+        window1.addComponent(new EmptySpace(1, 10), SizePolicy.CONSTANT);
+        window1.addComponent(new Label("Fullscreen window"));
+        window1.addComponent(new EmptySpace(1, 10), SizePolicy.CONSTANT);
+
+        Panel buttonPanel = new Panel(new Border.Invisible(), Panel.Orientation.HORISONTAL);
+        Button exitButton = new Button("Exit", new Action() {
+                public void doAction()  {
+                    guiScreen.closeWindow();
+                }
+            });
+        buttonPanel.addComponent(new EmptySpace(50, 1), SizePolicy.CONSTANT);
+        buttonPanel.addComponent(exitButton);
+        window1.addComponent(buttonPanel);
+        guiScreen.showWindow(window1, GUIScreen.Position.FULL_SCREEN);
+        guiScreen.getScreen().stopScreen();
+    } 
+}
