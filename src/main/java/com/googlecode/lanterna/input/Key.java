e932076f1a0e111809403deb7001240533c9f759
Martin Berglund <mabe02@gmail.com>
2012-07-31 21:43:05 +0900

    Adding constants for the F1-F12 function keys

--- src/main/java/com/googlecode/lanterna/input/Key.java
+++ src/main/java/com/googlecode/lanterna/input/Key.java
@@ -71,6 +71,18 @@ public class Key
         Tab('\t'),
         ReverseTab('/'),
         Enter('\n'),
+        F1('1'),
+        F2('2'),
+        F3('3'),
+        F4('4'),
+        F5('5'),
+        F6('6'),
+        F7('7'),
+        F8('8'),
+        F9('9'),
+        F10('Q'),   //No idea what to pick here, but it doesn't really matter
+        F11('W'),   //No idea what to pick here, but it doesn't really matter
+        F12('Y'),   //No idea what to pick here, but it doesn't really matter
         Unknown('!'),
         CursorLocation('£');
 
