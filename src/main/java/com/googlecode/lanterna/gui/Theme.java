d3b9d5307ab5b122afdf64d5e934b58480a7467c
Martin Berglund <mabe02@gmail.com>
2012-09-16 16:57:57 +0900

    Fixing bug with Theme where the static elements are not initialized in the correct order

--- src/main/java/com/googlecode/lanterna/gui/Theme.java
+++ src/main/java/com/googlecode/lanterna/gui/Theme.java
@@ -16,32 +16,28 @@
  * 
  * Copyright (C) 2010-2012 Martin
  */
-
 package com.googlecode.lanterna.gui;
 
+import com.googlecode.lanterna.terminal.Terminal.Color;
 import java.util.*;
 
-import com.googlecode.lanterna.terminal.Terminal.Color;
-
 /**
- * Extend this class to create your own themes. A {@code Theme} consists of
- * several {@code Theme.Definition}s, one for each {@code Theme.Category} value.
- * When components are setting their colors according to the theme, they do so
- * by calling {@code TextGraphics.applyTheme}.
+ * Extend this class to create your own themes. A {@code Theme} consists of several
+ * {@code Theme.Definition}s, one for each {@code Theme.Category} value. When components are setting
+ * their colors according to the theme, they do so by calling {@code TextGraphics.applyTheme}.
+ *
  * @author Martin
  */
 public class Theme {
-	private static final Theme DEFAULT_INSTANCE = new Theme();
-	
-	private static final Definition DEFAULT = new Definition(Color.BLACK, Color.WHITE, false);
-	private static final Definition SELECTED = new Definition(Color.WHITE, Color.BLUE, true);
-	private Map<Category,Definition> styles = new HashMap<Category,Definition>();
-	
-	/**
-	Represents things which can be styled.
-	*/
-    public enum Category
-    {
+    private static final Definition DEFAULT = new Definition(Color.BLACK, Color.WHITE, false);
+    private static final Definition SELECTED = new Definition(Color.WHITE, Color.BLUE, true);
+    private Map<Category, Definition> styles = new EnumMap<Category, Definition>(Category.class);
+    private static final Theme DEFAULT_INSTANCE = new Theme();
+
+    /**
+     * Represents things which can be styled.
+     */
+    public enum Category {
         DIALOG_AREA,
         SCREEN_BACKGROUND,
         SHADOW,
@@ -61,248 +57,229 @@ public class Theme {
         PROGRESS_BAR_REMAINING
     }
 
-    protected Theme()
-    {
-    	setDefinition(Category.DIALOG_AREA, DEFAULT);
-    	setDefinition(Category.SCREEN_BACKGROUND, new Definition(Color.CYAN, Color.BLUE, true));
-    	setDefinition(Category.SHADOW, new Definition(Color.BLACK, Color.BLACK, true));
-    	setDefinition(Category.BORDER, new Definition(Color.BLACK, Color.WHITE, true));
-    	setDefinition(Category.RAISED_BORDER, new Definition(Color.WHITE, Color.WHITE, true));
-    	setDefinition(Category.BUTTON_LABEL_ACTIVE, new Definition(Color.YELLOW, Color.BLUE, true));
-    	setDefinition(Category.BUTTON_LABEL_INACTIVE, new Definition(Color.BLACK, Color.WHITE, true));
-    	setDefinition(Category.BUTTON_ACTIVE, SELECTED);
-    	setDefinition(Category.BUTTON_INACTIVE, DEFAULT);
-    	setDefinition(Category.LIST_ITEM, DEFAULT);
-    	setDefinition(Category.LIST_ITEM_SELECTED, SELECTED);
-    	setDefinition(Category.CHECKBOX, DEFAULT);
-    	setDefinition(Category.CHECKBOX_SELECTED, SELECTED);
-    	setDefinition(Category.TEXTBOX, SELECTED);
-    	setDefinition(Category.TEXTBOX_FOCUSED, new Definition(Color.YELLOW, Color.BLUE, true));
-    	setDefinition(Category.PROGRESS_BAR_COMPLETED, new Definition(Color.GREEN, Color.BLACK, false));
-    	setDefinition(Category.PROGRESS_BAR_REMAINING, new Definition(Color.RED, Color.BLACK, false));
+    protected Theme() {
+        setDefinition(Category.DIALOG_AREA, DEFAULT);
+        setDefinition(Category.SCREEN_BACKGROUND, new Definition(Color.CYAN, Color.BLUE, true));
+        setDefinition(Category.SHADOW, new Definition(Color.BLACK, Color.BLACK, true));
+        setDefinition(Category.BORDER, new Definition(Color.BLACK, Color.WHITE, true));
+        setDefinition(Category.RAISED_BORDER, new Definition(Color.WHITE, Color.WHITE, true));
+        setDefinition(Category.BUTTON_LABEL_ACTIVE, new Definition(Color.YELLOW, Color.BLUE, true));
+        setDefinition(Category.BUTTON_LABEL_INACTIVE, new Definition(Color.BLACK, Color.WHITE, true));
+        setDefinition(Category.BUTTON_ACTIVE, SELECTED);
+        setDefinition(Category.BUTTON_INACTIVE, DEFAULT);
+        setDefinition(Category.LIST_ITEM, DEFAULT);
+        setDefinition(Category.LIST_ITEM_SELECTED, SELECTED);
+        setDefinition(Category.CHECKBOX, DEFAULT);
+        setDefinition(Category.CHECKBOX_SELECTED, SELECTED);
+        setDefinition(Category.TEXTBOX, SELECTED);
+        setDefinition(Category.TEXTBOX_FOCUSED, new Definition(Color.YELLOW, Color.BLUE, true));
+        setDefinition(Category.PROGRESS_BAR_COMPLETED, new Definition(Color.GREEN, Color.BLACK, false));
+        setDefinition(Category.PROGRESS_BAR_REMAINING, new Definition(Color.RED, Color.BLACK, false));
     }
 
-    
-    public Theme.Definition getDefinition(Category category)
-    {
-    	if (styles.containsKey(category) && styles.get(category) != null)
-    		return styles.get(category);
-    	
-    	return getDefault();
+    public Theme.Definition getDefinition(Category category) {
+        if (styles.containsKey(category) && styles.get(category) != null) {
+            return styles.get(category);
+        }
+
+        return getDefault();
     }
 
     protected void setDefinition(Category category, Definition def) {
-    	if (def == null)
-    		styles.remove(category);
-    	else
-    		styles.put(category, def);
+        if (def == null) {
+            styles.remove(category);
+        } else {
+            styles.put(category, def);
+        }
     }
-    
-	/**
-    Gets the default style to use when no Category-specific style is set.
-	*/
-	protected Definition getDefaultStyle() {
+
+    /**
+     * Gets the default style to use when no Category-specific style is set.
+     */
+    protected Definition getDefaultStyle() {
         return DEFAULT;
     }
-    
-    /**
-    @deprecated use @{code getDefaultStyle()} instead
-    */
-    @Deprecated
-    protected Definition getDefault()
-    {
-    	return getDefaultStyle();
-    }
 
     /**
-    @deprecated use {@code getDefinition} instead.
-    */
+     * @deprecated use
+     * @{code getDefaultStyle()} instead
+     */
     @Deprecated
-    protected Definition getDialogEmptyArea()
-    {
-    	return getDefinition(Category.DIALOG_AREA);
-    }
-
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
-	@Deprecated
-	protected Definition getScreenBackground()
-    {
-    	return getDefinition(Category.SCREEN_BACKGROUND);
-    }
-
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
-    @Deprecated
-    protected Definition getShadow()
-    {
-    	return getDefinition(Category.SHADOW);
+    protected Definition getDefault() {
+        return getDefaultStyle();
     }
 
     /**
-    @deprecated use {@code getDefinition} instead.
-    */
-	@Deprecated
-    protected Definition getBorder()
-    {
-    	return getDefinition(Category.BORDER);
+     * @deprecated use {@code getDefinition} instead.
+     */
+    @Deprecated
+    protected Definition getDialogEmptyArea() {
+        return getDefinition(Category.DIALOG_AREA);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getRaisedBorder()
-    {
+    protected Definition getScreenBackground() {
+        return getDefinition(Category.SCREEN_BACKGROUND);
+    }
+
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
+    @Deprecated
+    protected Definition getShadow() {
+        return getDefinition(Category.SHADOW);
+    }
+
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
+    @Deprecated
+    protected Definition getBorder() {
+        return getDefinition(Category.BORDER);
+    }
+
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
+    @Deprecated
+    protected Definition getRaisedBorder() {
         return getDefinition(Category.RAISED_BORDER);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getButtonLabelActive()
-    {
+    protected Definition getButtonLabelActive() {
         return getDefinition(Category.BUTTON_LABEL_ACTIVE);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getButtonLabelInactive()
-    {
+    protected Definition getButtonLabelInactive() {
         return getDefinition(Category.BUTTON_LABEL_INACTIVE);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getButtonActive()
-    {
+    protected Definition getButtonActive() {
         return getDefinition(Category.BUTTON_ACTIVE);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getButtonInactive()
-    {
+    protected Definition getButtonInactive() {
         return getDefinition(Category.BUTTON_INACTIVE);
     }
-    
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getItem()
-    {
+    protected Definition getItem() {
         return getDefinition(Category.LIST_ITEM);
     }
-    
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getItemSelected()
-    {
+    protected Definition getItemSelected() {
         return getDefinition(Category.LIST_ITEM_SELECTED);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getCheckBox()
-    {
+    protected Definition getCheckBox() {
         return getDefinition(Category.CHECKBOX);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getCheckBoxSelected()
-    {
+    protected Definition getCheckBoxSelected() {
         return getDefinition(Category.CHECKBOX_SELECTED);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getTextBoxFocused()
-    {
+    protected Definition getTextBoxFocused() {
         return getDefinition(Category.TEXTBOX_FOCUSED);
     }
 
-	/**
-    @deprecated use {@code getDefinition} instead.
-    */
+    /**
+     * @deprecated use {@code getDefinition} instead.
+     */
     @Deprecated
-    protected Definition getTextBox()
-    {
+    protected Definition getTextBox() {
         return getDefinition(Category.TEXTBOX);
     }
 
-    public static Theme getDefaultTheme()
-    {
+    public static Theme getDefaultTheme() {
         return DEFAULT_INSTANCE;
     }
 
     /**
-    A style definition encompassing colors and effects.
-    */
-    public static class Definition
-    {
+     * A style definition encompassing colors and effects.
+     */
+    public static class Definition {
+
         private Color foreground;
         private Color background;
         private boolean highlighted;
         private boolean underlined;
 
-        public Definition(Color foreground, Color background)
-        {
+        public Definition(Color foreground, Color background) {
             this(foreground, background, false);
         }
 
-        public Definition(Color foreground, Color background, boolean highlighted)
-        {
+        public Definition(Color foreground, Color background, boolean highlighted) {
             this(foreground, background, highlighted, false);
         }
 
-        public Definition(Color foreground, Color background, boolean highlighted, boolean underlined)
-        {
-        	if (foreground == null)
-        		throw new IllegalArgumentException("foreground color cannot be null");
-        	
-        	if (background == null)
-        		throw new IllegalArgumentException("background color cannot be null");
-        	
+        public Definition(Color foreground, Color background, boolean highlighted, boolean underlined) {
+            if (foreground == null) {
+                throw new IllegalArgumentException("foreground color cannot be null");
+            }
+
+            if (background == null) {
+                throw new IllegalArgumentException("background color cannot be null");
+            }
+
             this.foreground = foreground;
             this.background = background;
             this.highlighted = highlighted;
             this.underlined = underlined;
         }
-        
+
         public Color foreground() {
-        	return foreground;
+            return foreground;
         }
-        
+
         public Color background() {
-        	return background;
+            return background;
         }
-        
+
         public boolean isHighlighted() {
-        	return highlighted;
+            return highlighted;
         }
-        
+
         public boolean isUnderlined() {
-        	return underlined;
+            return underlined;
         }
-        
     }
 }
