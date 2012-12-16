f5cd3531e5309abc102800cb352c427babe76da7
Martin Berglund <mabe02@gmail.com>
2012-12-16 22:48:12 +0900

    Adding more explaination

--- src/test/java/com/googlecode/lanterna/test/issue/Issue53.java
+++ src/test/java/com/googlecode/lanterna/test/issue/Issue53.java
@@ -5,11 +5,12 @@ import com.googlecode.lanterna.gui.dialog.TextInputDialog;
 import com.googlecode.lanterna.test.TestTerminalFactory;
 
 public class Issue53 {
-    
     public static void main(String[] args) {
         final GUIScreen guiScreen = new TestTerminalFactory(args).createGUIScreen();
         guiScreen.getScreen().startScreen();
-        TextInputDialog.showTextInputBox(guiScreen, "Testing issue 53", "This is a test for issue 53", "");
+        //Ensuring that a TextInputBox doesn't throw an exception if the description is
+        //shorter than the title
+        TextInputDialog.showTextInputBox(guiScreen, "Testing issue 53", "Issue 53", "");
         guiScreen.getScreen().stopScreen();
     }
 }
