7cb5d88546fa64846e86be39ad6000db141e8b6d
Martin Berglund <mabe02@gmail.com>
2013-01-02 21:27:27 +0900

    Promoting a few private and package private methods to protected to make class overloading easier (Issue 56)

--- src/main/java/com/googlecode/lanterna/gui/Window.java
+++ src/main/java/com/googlecode/lanterna/gui/Window.java
@@ -238,7 +238,7 @@ public class Window
             return currentlyInFocus.getHotspot();
     }
 
-    void onKeyPressed(Key key)
+    protected void onKeyPressed(Key key)
     {
         if(currentlyInFocus != null) {
             Interactable.Result result =  currentlyInFocus.keyboardInteraction(key);
@@ -278,7 +278,7 @@ public class Window
         }
     }
     
-    private void onUnhandledKeyPress(Key key) {
+    protected void onUnhandledKeyPress(Key key) {
         for(WindowListener listener: windowListeners)
             listener.onUnhandledKeyboardInteraction(this, key);
     }
@@ -297,12 +297,12 @@ public class Window
         this.soloWindow = soloWindow;
     }
 
-    boolean maximisesVertically()
+    protected boolean maximisesVertically()
     {
         return contentPane.maximisesVertically();
     }
 
-    boolean maximisesHorisontally()
+    protected boolean maximisesHorisontally()
     {
         return contentPane.maximisesHorisontally();
     }
