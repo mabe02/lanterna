81456ca6d9c2a0ed0c11c6049043e708b130f3f4
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-01-30 01:06:46 +0100

    Implemented a Popup version of the RadioCheckBoxList When not in popup mode, displays currently checked item on one line, opens to full size on Enter/Space

---
+++ src/main/java/com/googlecode/lanterna/gui/component/PopupCheckBoxList.java
@@ -0,0 +1,68 @@
+package com.googlecode.lanterna.gui.component;
+
+import com.googlecode.lanterna.gui.TextGraphics;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.ACS;
+import com.googlecode.lanterna.terminal.TerminalPosition;
+import com.googlecode.lanterna.terminal.TerminalSize;
+
+public class PopupCheckBoxList extends RadioCheckBoxList {
+
+	private static final String EMPTY_SELECT = "-------";
+	
+	private boolean poppedUp = false;
+	
+	public boolean isPoppedUp() {
+		return poppedUp;
+	}
+
+	@Override
+	protected String createItemString(int index) {
+		if (poppedUp) {
+			return super.createItemString(index);
+		}
+		return ACS.ARROW_DOWN + this.getItemAt(index).toString();
+	}
+	
+	@Override
+	public void repaint(TextGraphics graphics) {
+		if (poppedUp) {
+			super.repaint(graphics);
+		} else {
+			if (hasFocus()) graphics.applyTheme(getListItemThemeDefinition(graphics.getTheme()));
+			graphics.drawString(0, 0, createItemString(getCheckedItemIndex()));
+			setHotspot(graphics.translateToGlobalCoordinates(new TerminalPosition(0, 0)));
+		}
+	}
+	
+	@Override
+	public Result keyboardInteraction(Key key) {
+		if (poppedUp) {
+			Result parentRet = super.keyboardInteraction(key);
+			if (key.equalsString("<cr>") || key.equalsString("<space>")) {
+				poppedUp = false;
+				invalidate();
+				return parentRet;
+			}
+		} 
+		if (key.equalsString("<cr>")) {
+			poppedUp = true;
+			invalidate();
+		} else if (key.equalsString("<Tab>")) {
+			return Result.NEXT_INTERACTABLE_DOWN;
+		} else if (key.equalsString("<S-Tab>")) {
+			return Result.PREVIOUS_INTERACTABLE_UP;
+		}
+		return Result.EVENT_HANDLED;
+	}
+
+	@Override
+	public TerminalSize getPreferredSize() {
+		if (poppedUp) {
+			return super.getPreferredSize();
+		}
+		int width = getSelectedIndex() > -1 ? createItemString(getSelectedIndex()).length() : EMPTY_SELECT.length();
+		return new TerminalSize(width+1, 1);
+	}
+	
+}
