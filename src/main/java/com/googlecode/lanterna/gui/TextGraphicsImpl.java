4f47323b7586e26e8095ecf506c598cb3a6f9794
Martin Berglund <mabe02@gmail.com>
2014-02-25 21:49:18 +0900

    Simplifying

--- src/main/java/com/googlecode/lanterna/gui/TextGraphicsImpl.java
+++ src/main/java/com/googlecode/lanterna/gui/TextGraphicsImpl.java
@@ -54,8 +54,8 @@ class TextGraphicsImpl implements TextGraphics
         this.screen = screen;
         this.theme = theme;
         this.currentlyBold = false;
-        this.foregroundColor = TextColor.fromOldFormat(Terminal.ANSIColor.DEFAULT);
-        this.backgroundColor = TextColor.fromOldFormat(Terminal.ANSIColor.DEFAULT);
+        this.foregroundColor = TextColor.ANSI.DEFAULT;
+        this.backgroundColor = TextColor.ANSI.DEFAULT;
     }
 
     private TextGraphicsImpl(final TextGraphicsImpl graphics, final TerminalPosition topLeft, final TerminalSize areaSize)
