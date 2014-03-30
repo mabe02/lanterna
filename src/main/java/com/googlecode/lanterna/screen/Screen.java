13d928d4b2940c54c2745fe88891ea4d7145596e
Martin Berglund <mabe02@gmail.com>
2014-03-30 21:54:11 +0900

    Moving stuff around inside Screen

--- src/main/java/com/googlecode/lanterna/screen/Screen.java
+++ src/main/java/com/googlecode/lanterna/screen/Screen.java
@@ -14,23 +14,31 @@ import java.io.IOException;
 import java.util.Set;
 
 /**
+ * A layer to put on top of a Terminal object, giving you a kind of screen buffer to use, which is a lot easier to work
+ * with. Drawing text or graphics to the terminal is kind of like writing to a bitmap.
  *
- * @author martin
+ * @author Martin
  */
 public interface Screen {
 
     /**
      * Erases all the characters on the screen, effectively giving you a blank area. The default background color will
-     * be used, if you want to fill the screen with a different color you will need to do this manually.
+     * be used. This is effectively the same as calling 
+     * "fill(TerminalPosition.TOP_LEFT_CORNER, getSize(), TextColor.ANSI.Default)".
+     * <p/>
+     * Please note that calling this method will only affect the back buffer, you need to call refresh to make the 
+     * change visible.
      */
     void clear();
-
+    
     /**
-     * Clears the terminal and repaints with the whole content of the Screen. This is useful of something has written to
-     * the terminal outside of the Screen (System.out or through direct calls to the underlying Terminal) and you want
-     * to make sure that the content of Screen is completely pushed to the terminal.
+     * Takes a rectangle on the screen and fills it with a particular color. Please note that calling this method will 
+     * only affect the back buffer, you need to call refresh to make the change visible.
+     * @param topLeft The top-left (inclusive) coordinate of the top left corner of the rectangle
+     * @param size Size (in columns and rows) of the area to draw
+     * @param color Color to draw the rectangle with
      */
-    void completeRefresh();
+    void fill(TerminalPosition topLeft, TerminalSize size, TextColor color);
 
     /**
      * @return Position where the cursor will be located after the screen has been refreshed or {@code null} if the
@@ -80,13 +88,28 @@ public interface Screen {
     KeyStroke readInput() throws IOException;
 
     /**
-     * Call this method to make changes done through {@code putCharacter(...)},
-     * {@code putString(...)} visible on the terminal. The screen will calculate the changes that are required and send
-     * the necessary characters and control sequences to make it so. If the terminal has been resized since the last
-     * refresh, and no call to {@code doResize()} has been made, this method will resize the internal buffer and fill
+     * Call this method to make changes done to the backbuffer visible on the screen. The screen will calculate the 
+     * changes that are required and send the necessary characters and control sequences to make it so. If the terminal 
+     * has been resized since the last refresh, and no call to {@code doResize()} has been made, this method will resize the internal buffer and fill
      * the extra space with a padding character.
+     * <p/>
+     * Note, calling this is the same as calling refresh(false);
      */
     void refresh();
+    
+    /**
+     * Call this method to make changes done to the backbuffer visible on the screen. There are two ways of doing this,
+     * either by clearing the whole terminal and re-drawing the backbuffer to the terminal. That is called a complete 
+     * refresh and will be used if you pass in true to the completeRefresh parameter. The other way is to calculate the
+     * diff between what's currently visible and what's in the backbuffer, then generate what operations that needs to
+     * be done to make the terminal display the backbuffer.
+     * <p/>
+     * In general, if you have changed the screen a lot since the last refresh, it makes sense do a complete refresh,
+     * but if only a couple of locations are different then you should probably use the diff method.
+     * @param completeRefresh If true, will do a complete redraw of the terminal, otherwise it will calculate a diff 
+     * between the screen and the back-buffer and only send the changes required to make the terminal like the buffer
+     */
+    void refresh(boolean completeRefresh);
 
     /**
      * This method will check if there are any resize commands pending. If true, you need to call refresh() to perform
