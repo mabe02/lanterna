26981500f7356317888c14c1d8c4dac32d6410d7
Martin Berglund <mabe02@gmail.com>
2014-06-09 21:39:23 +0900

    Backporting Linux console function key codes from 3.0

--- src/main/java/com/googlecode/lanterna/input/CommonProfile.java
+++ src/main/java/com/googlecode/lanterna/input/CommonProfile.java
@@ -59,14 +59,19 @@ public class CommonProfile extends KeyMappingProfile
                     new BasicCharacterPattern(new Key(Key.Kind.PageDown, false, true), ESC_CODE, ESC_CODE, '[', '6', '~'),
                     new BasicCharacterPattern(new Key(Key.Kind.F1), ESC_CODE, 'O', 'P'),    //Cygwin
                     new BasicCharacterPattern(new Key(Key.Kind.F1), ESC_CODE, '[', '1', '1', '~'),
+                    new BasicCharacterPattern(new Key(Key.Kind.F1), ESC_CODE, '[', '[', 'A'), //Linux
                     new BasicCharacterPattern(new Key(Key.Kind.F2), ESC_CODE, 'O', 'Q'),    //Cygwin
                     new BasicCharacterPattern(new Key(Key.Kind.F2), ESC_CODE, '[', '1', '2', '~'),
+                    new BasicCharacterPattern(new Key(Key.Kind.F2), ESC_CODE, '[', '[', 'B'), //Linux
                     new BasicCharacterPattern(new Key(Key.Kind.F3), ESC_CODE, 'O', 'R'),    //Cygwin
                     new BasicCharacterPattern(new Key(Key.Kind.F3), ESC_CODE, '[', '1', '3', '~'),
+                    new BasicCharacterPattern(new Key(Key.Kind.F3), ESC_CODE, '[', '[', 'C'), //Linux
                     new BasicCharacterPattern(new Key(Key.Kind.F4), ESC_CODE, 'O', 'S'),    //Cygwin
                     new BasicCharacterPattern(new Key(Key.Kind.F4), ESC_CODE, '[', '1', '4', '~'),
+                    new BasicCharacterPattern(new Key(Key.Kind.F4), ESC_CODE, '[', '[', 'D'), //Linux
                     new BasicCharacterPattern(new Key(Key.Kind.F5), ESC_CODE, '[', '1', '5', '~'),
                     new BasicCharacterPattern(new Key(Key.Kind.F5), ESC_CODE, '[', '1', '6', '~'),
+                    new BasicCharacterPattern(new Key(Key.Kind.F5), ESC_CODE, '[', '[', 'E'), //Linux
                     new BasicCharacterPattern(new Key(Key.Kind.F5), ESC_CODE, '[', '3', '2', '~'),
                     new BasicCharacterPattern(new Key(Key.Kind.F6), ESC_CODE, '[', '1', '7', '~'),
                     new BasicCharacterPattern(new Key(Key.Kind.F7), ESC_CODE, '[', '1', '8', '~'),
