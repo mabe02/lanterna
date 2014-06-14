71b6306312b6e3a4af018aab51e7523d93a62b3e
Martin Berglund <mabe02@gmail.com>
2014-06-14 18:04:26 +0900

    Clarifying

--- src/main/java/com/googlecode/lanterna/terminal/text/ANSITerminal.java
+++ src/main/java/com/googlecode/lanterna/terminal/text/ANSITerminal.java
@@ -27,7 +27,7 @@ import java.io.OutputStream;
 import java.nio.charset.Charset;
 
 /**
- * A common ANSI text terminal implementation
+ * Class containing common code for ANSI compliant text terminals and terminal emulators.
  *
  * @see <a href="http://en.wikipedia.org/wiki/ANSI_escape_code">Wikipedia</a>
  * @author Martin
