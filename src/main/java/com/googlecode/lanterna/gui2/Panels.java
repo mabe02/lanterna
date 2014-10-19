a6c8036137702eaf2772f7296d894e707cd46bbe
Martin Berglund <mabe02@gmail.com>
2014-10-19 18:21:48 +0900

    Adding a utility class for quickly create panels surrounding a number of components

---
+++ src/main/java/com/googlecode/lanterna/gui2/Panels.java
@@ -0,0 +1,29 @@
+package com.googlecode.lanterna.gui2;
+
+/**
+ * Utility class for quickly bunching up components in a panel, arranged in a particular pattern
+ * @author Martin
+ */
+public class Panels {
+
+    public static Panel horizontal(Component... components) {
+        Panel panel = new Panel();
+        panel.setLayoutManager(new LinearLayout(LinearLayout.Direction.HORIZONTAL));
+        for(Component component: components) {
+            panel.addComponent(component);
+        }
+        return panel;
+    }
+
+    public static Panel vertical(Component... components) {
+        Panel panel = new Panel();
+        panel.setLayoutManager(new LinearLayout(LinearLayout.Direction.VERTICAL));
+        for(Component component: components) {
+            panel.addComponent(component);
+        }
+        return panel;
+    }
+
+    //Cannot instantiate
+    private Panels() {}
+}
