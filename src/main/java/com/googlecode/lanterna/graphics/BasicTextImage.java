9393f2673cf2773a77f641d94e9af6a92fe8c77b
Martin Berglund <mabe02@gmail.com>
2014-11-16 16:53:35 +0900

    Adding something that looks a bit like BufferedImage from AWT, a class that can store in-memory text graphics data that can be easily reused with TextGraphics

---
+++ src/main/java/com/googlecode/lanterna/graphics/BasicTextImage.java
@@ -0,0 +1,164 @@
+/*
+ * This file is part of lanterna (http://code.google.com/p/lanterna/).
+ *
+ * lanterna is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2010-2014 Martin
+ */
+package com.googlecode.lanterna.graphics;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+
+/**
+ * Simple implementation of TextImage that keeps the content as a two-dimensional TextCharacter array. If you copy 
+ * between two BasicTextImage:s, they can use {@code System.arrayCopy} to make the operation faster(?).
+ * @author martin
+ */
+public class BasicTextImage implements TextImage {
+    private final TextCharacter[][] buffer;
+    
+    /**
+     * Creates a new BasicTextImage with a given size and a TextCharacter to initially fill it with
+     * @param size Size of the image
+     * @param initialContent What character to set as the initial content
+     */
+    public BasicTextImage(TerminalSize size, TextCharacter initialContent) {
+        this(size, new TextCharacter[0][], initialContent);
+    }    
+    
+    /**
+     * Creates a new BasicTextImage by copying a region of a two-dimensional array of TextCharacter:s. If the area to be 
+     * copied to larger than the source array, a filler character is used.
+     * @param size Size to create the new BasicTextImage as (and size to copy from the array)
+     * @param toCopy Array to copy initial data from
+     * @param initialContent Filler character to use if the source array is smaller than the requested size
+     */
+    private BasicTextImage(TerminalSize size, TextCharacter[][] toCopy, TextCharacter initialContent) {
+        if(size == null || toCopy == null || initialContent == null) {
+            throw new IllegalArgumentException("Cannot create BasicTextImage with null " +
+                    (size == null ? "size" : (toCopy == null ? "toCopy" : "filler")));
+        }
+        int rows = size.getRows();
+        int columns = size.getColumns();
+        buffer = new TextCharacter[rows][];
+        for(int y = 0; y < rows; y++) {
+            buffer[y] = new TextCharacter[columns];
+            for(int x = 0; x < columns; x++) {
+                if(y < toCopy.length && x < toCopy[y].length) {
+                    buffer[y][x] = toCopy[y][x];
+                }
+                else {
+                    buffer[y][x] = initialContent;
+                }
+            }
+        }
+    }
+    
+    @Override
+    public void setAll(TextCharacter character) {
+        if(character == null) {
+            throw new IllegalArgumentException("Cannot call BasicTextImage.setAll(..) with null character");
+        }
+        for(TextCharacter[] line : buffer) {
+            for(int x = 0; x < line.length; x++) {
+                line[x] = character;
+            }
+        }
+    }
+
+    @Override
+    public BasicTextImage resize(TerminalSize newSize, TextCharacter filler) {
+        if(newSize == null || filler == null) {
+            throw new IllegalArgumentException("Cannot resize BasicTextImage with null " +
+                    (newSize == null ? "newSize" : "filler"));
+        }
+        if(newSize.getRows() == buffer.length &&
+                (buffer.length == 0 || newSize.getColumns() == buffer[0].length)) {
+            return this;
+        }
+        return new BasicTextImage(newSize, buffer, filler);
+    }
+
+    @Override
+    public void setCharacterAt(TerminalPosition position, TextCharacter character) {
+        if(position == null) {
+            throw new IllegalArgumentException("Cannot call BasicTextImage.setCharacterAt(..) with null position");
+        }
+        setCharacterAt(position.getColumn(), position.getRow(), character);
+    }
+    
+    @Override
+    public void setCharacterAt(int column, int row, TextCharacter character) {
+        if(character == null) {
+            throw new IllegalArgumentException("Cannot call BasicTextImage.setCharacterAt(..) with null character");
+        }
+        if(column < 0 || row < 0 || row >= buffer.length || column >= buffer[0].length) {
+            return;
+        }
+        
+        buffer[row][column] = character;
+    }
+
+    @Override
+    public TextCharacter getCharacterAt(TerminalPosition position) {
+        if(position == null) {
+            throw new IllegalArgumentException("Cannot call BasicTextImage.getCharacterAt(..) with null position");
+        }
+        return getCharacterAt(position.getColumn(), position.getRow());
+    }
+    
+    @Override
+    public TextCharacter getCharacterAt(int column, int row) {
+        if(column < 0 || row < 0 || row >= buffer.length || column >= buffer[0].length) {
+            return null;
+        }
+        
+        return buffer[row][column];
+    }
+    
+    @Override
+    public void copyTo(TextImage destination) {
+        copyTo(destination, 0, buffer.length, 0, buffer[0].length, 0, 0);
+    }
+
+    @Override
+    public void copyTo(
+            TextImage destination,
+            int startRowIndex,
+            int rows,
+            int startColumnIndex,
+            int columns,
+            int destinationRowOffset,
+            int destinationColumnOffset) {
+
+        if(destination instanceof BasicTextImage) {
+            for(int y = startRowIndex; y < startRowIndex + rows; y++) {
+                System.arraycopy(buffer[y], startColumnIndex, ((BasicTextImage)destination).buffer[y - startRowIndex + destinationRowOffset], destinationColumnOffset, columns);
+            }
+        }
+        else {
+            //Manually copy character by character
+            for(int y = startRowIndex; y < startRowIndex + rows; y++) {
+                for(int x = startColumnIndex; x < startColumnIndex + columns; x++) {
+                    destination.setCharacterAt(
+                            x - startColumnIndex + destinationColumnOffset, 
+                            y - startRowIndex + destinationRowOffset, 
+                            buffer[y][x]);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/googlecode/lanterna/graphics/TextImage.java
@@ -0,0 +1,106 @@
+/*
+ * This file is part of lanterna (http://code.google.com/p/lanterna/).
+ *
+ * lanterna is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2010-2014 Martin
+ */
+package com.googlecode.lanterna.graphics;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+
+/**
+ * An 'image' build up of text characters with color and style information. These are completely in memory and not 
+ * visible anyway, but can be used when drawing with a TextGraphics objects. 
+ * @author martin
+ */
+public interface TextImage {
+    /**
+     * Returns the character stored at a particular position in this image
+     * @param position Coordinates of the character
+     * @return TextCharacter stored at the specified position
+     */
+    TextCharacter getCharacterAt(TerminalPosition position);
+    
+    /**
+     * Returns the character stored at a particular position in this image
+     * @param column Column coordinate of the character
+     * @param row Row coordinate of the character
+     * @return TextCharacter stored at the specified position
+     */
+    TextCharacter getCharacterAt(int column, int row);
+            
+    /**
+     * Sets the character at a specific position in the image to a particular TextCharacter. If the position is outside
+     * of the image's size, this method does nothing.
+     * @param position Coordinates of the character
+     * @param character What TextCharacter to assign at the specified position
+     */
+    void setCharacterAt(TerminalPosition position, TextCharacter character);
+            
+    /**
+     * Sets the character at a specific position in the image to a particular TextCharacter. If the position is outside
+     * of the image's size, this method does nothing.
+     * @param column Column coordinate of the character
+     * @param row Row coordinate of the character
+     * @param character What TextCharacter to assign at the specified position
+     */
+    void setCharacterAt(int column, int row, TextCharacter character);
+    
+    /**
+     * Sets the text image content to one specified character (including color and style)
+     * @param character The character to fill the image with
+     */
+    void setAll(TextCharacter character);
+    
+    /**
+     * Returns a copy of this image resized to a new size and using a specified filler character if the new size is 
+     * larger than the old and we need to fill in empty areas. The copy will be independent from the one this method is 
+     * invoked on, so modifying one will not affect the other.
+     * @param newSize Size of the new image
+     * @param filler Filler character to use on the new areas when enlarging the image (is not used when shrinking)
+     * @return Copy of this image, but resized
+     */
+    TextImage resize(TerminalSize newSize, TextCharacter filler);
+    
+    
+    /**
+     * Copies this TextImage's content to another TextImage. If the destination TextImage is larger than this 
+     * ScreenBuffer, the areas outside of the area that is written to will be untouched.
+     * @param destination TextImage to copy to
+     */
+    void copyTo(TextImage destination);
+    
+    /**
+     * Copies this TextImage's content to another TextImage. If the destination TextImage is larger than this 
+     * TextImage, the areas outside of the area that is written to will be untouched.
+     * @param destination TextImage to copy to
+     * @param startRowIndex Which row in this image to copy from
+     * @param rows How many rows to copy
+     * @param startColumnIndex Which column in this image to copy from
+     * @param columns How many columns to copy
+     * @param destinationRowOffset Offset (in number of rows) in the target image where we want to first copied row to be
+     * @param destinationColumnOffset Offset (in number of columns) in the target image where we want to first copied column to be
+     */
+    void copyTo(
+            TextImage destination,
+            int startRowIndex,
+            int rows,
+            int startColumnIndex,
+            int columns,
+            int destinationRowOffset,
+            int destinationColumnOffset);
+}
