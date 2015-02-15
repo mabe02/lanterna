8a4c919b4a9f88707f971f61eaba4c2dca476cfd
Martin Berglund <mabe02@gmail.com>
2015-02-15 17:59:11 +0900

    Implement window manager logic to run on every rendering

--- src/main/java/com/googlecode/lanterna/gui2/DefaultWindowManager.java
+++ src/main/java/com/googlecode/lanterna/gui2/DefaultWindowManager.java
@@ -12,13 +12,28 @@ import java.util.List;
 public class DefaultWindowManager implements WindowManager {
 
     private final WindowDecorationRenderer windowDecorationRenderer;
+    private TerminalSize lastKnownScreenSize;
 
     public DefaultWindowManager() {
         this(new DefaultWindowDecorationRenderer());
     }
 
     public DefaultWindowManager(WindowDecorationRenderer windowDecorationRenderer) {
+        this(windowDecorationRenderer, null);
+    }
+
+    public DefaultWindowManager(TerminalSize initialScreenSize) {
+        this(new DefaultWindowDecorationRenderer(), initialScreenSize);
+    }
+
+    public DefaultWindowManager(WindowDecorationRenderer windowDecorationRenderer, TerminalSize initialScreenSize) {
         this.windowDecorationRenderer = windowDecorationRenderer;
+        if(initialScreenSize != null) {
+            this.lastKnownScreenSize = initialScreenSize;
+        }
+        else {
+            this.lastKnownScreenSize = new TerminalSize(80, 24);
+        }
     }
 
     @Override
@@ -36,15 +51,27 @@ public class DefaultWindowManager implements WindowManager {
 
     @Override
     public void onAdded(WindowBasedTextGUI textGUI, Window window, List<Window> allWindows) {
-        if(allWindows.isEmpty()) {
-            window.setPosition(TerminalPosition.OFFSET_1x1);
-        }
-        else {
-            window.setPosition(allWindows.get(allWindows.size() - 1).getPosition().withRelative(2, 1));
-        }
         WindowDecorationRenderer decorationRenderer = getWindowDecorationRenderer(window);
         TerminalSize expectedDecoratedSize = decorationRenderer.getDecoratedSize(window, window.getPreferredSize());
         window.setDecoratedSize(expectedDecoratedSize);
+
+        if(allWindows.isEmpty()) {
+            window.setPosition(TerminalPosition.OFFSET_1x1);
+        }
+        else if(window.getHints().contains(Window.Hint.CENTERED)) {
+            int left = (lastKnownScreenSize.getColumns() - expectedDecoratedSize.getColumns()) / 2;
+            int top = (lastKnownScreenSize.getRows() - expectedDecoratedSize.getRows()) / 2;
+            window.setPosition(new TerminalPosition(left, top));
+        }
+        else {
+            TerminalPosition nextPosition = allWindows.get(allWindows.size() - 1).getPosition().withRelative(2, 1);
+            if(nextPosition.getColumn() + expectedDecoratedSize.getColumns() > lastKnownScreenSize.getColumns() ||
+                    nextPosition.getRow() + expectedDecoratedSize.getRows() > lastKnownScreenSize.getRows()) {
+                nextPosition = TerminalPosition.OFFSET_1x1;
+            }
+            window.setPosition(nextPosition);
+
+        }
     }
 
     @Override
@@ -54,11 +81,37 @@ public class DefaultWindowManager implements WindowManager {
 
     @Override
     public void prepareWindows(WindowBasedTextGUI textGUI, List<Window> allWindows, TerminalSize screenSize) {
+        this.lastKnownScreenSize = screenSize;
         for(Window window: allWindows) {
-            //Recalculate size + decorations
-            //Adjust if hinted
-            //Center if hinted
-            //Update window properties
+            WindowDecorationRenderer decorationRenderer = getWindowDecorationRenderer(window);
+            TerminalSize size = decorationRenderer.getDecoratedSize(window, window.getPreferredSize());
+            TerminalPosition position = window.getPosition();
+
+            if(window.getHints().contains(Window.Hint.FIT_TERMINAL_WINDOW) ||
+                    window.getHints().contains(Window.Hint.CENTERED)) {
+                //If the window is too big for the terminal, move it up towards 0x0 and if that's not enough then shrink
+                //it instead
+                while(position.getRow() > 0 && position.getRow() + size.getRows() > screenSize.getRows()) {
+                    position = position.withRelativeRow(-1);
+                }
+                while(position.getColumn() > 0 && position.getColumn() + size.getColumns() > screenSize.getColumns()) {
+                    position = position.withRelativeColumn(-1);
+                }
+                if(position.getRow() + size.getRows() > screenSize.getRows()) {
+                    size = size.withRows(screenSize.getRows() - position.getRow());
+                }
+                if(position.getColumn() + size.getColumns() > screenSize.getColumns()) {
+                    size = size.withColumns(screenSize.getColumns() - position.getColumn());
+                }
+            }
+            if(window.getHints().contains(Window.Hint.CENTERED)) {
+                int left = (lastKnownScreenSize.getColumns() - size.getColumns()) / 2;
+                int top = (lastKnownScreenSize.getRows() - size.getRows()) / 2;
+                position = new TerminalPosition(left, top);
+            }
+
+            window.setPosition(position);
+            window.setDecoratedSize(size);
         }
     }
 }
