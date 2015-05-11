2d845d39b68ef6de735106ff047980c5aa5c3a35
Martin Berglund <mabe02@gmail.com>
2015-05-11 21:21:41 +0900

    Completing initial implementation

--- src/main/java/com/googlecode/lanterna/gui2/GridLayout.java
+++ src/main/java/com/googlecode/lanterna/gui2/GridLayout.java
@@ -18,6 +18,7 @@
  */
 package com.googlecode.lanterna.gui2;
 
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 
 import java.util.*;
@@ -139,7 +140,295 @@ public class GridLayout implements LayoutManager {
 
     @Override
     public void doLayout(TerminalSize area, List<Component> components) {
+        Component[][] table = buildTable(components);
+        Map<Component, TerminalSize> sizeMap = new IdentityHashMap<Component, TerminalSize>();
+        Map<Component, TerminalPosition> positionMap = new IdentityHashMap<Component, TerminalPosition>();
 
+
+        //Figure out each column first, this can be done independently of the row heights
+        int[] columnWidths = getPreferredColumnWidths(table);
+
+        //Take notes of which columns we can expand if the usable area is larger than what the components want
+        Set<Integer> expandableColumns = getExpandableColumns(table);
+
+        //Next, start shrinking to make sure it fits the size of the area we are trying to lay out on
+        int totalWidth = shrinkWidthToFitArea(area, columnWidths);
+
+        //Finally, if there is extra space, make the expandable columns larger
+        while(area.getColumns() > totalWidth && !expandableColumns.isEmpty()) {
+            totalWidth = grabExtraHorizontalSpace(area, columnWidths, expandableColumns, totalWidth);
+        }
+
+        //Now repeat for rows
+        int[] rowHeights = getPreferredRowHeights(table);
+        Set<Integer> expandableRows = getExpandableRows(table);
+        int totalHeight = shrinkHeightToFitArea(area, rowHeights);
+        while(area.getRows() > totalHeight && !expandableRows.isEmpty()) {
+            totalHeight = grabExtraVerticalSpace(area, rowHeights, expandableRows, totalHeight);
+        }
+
+        //Ok, all constraints are in place, we can start placing out components. To simplify, do it horizontally first
+        //and vertically after
+        TerminalPosition tableCellTopLeft = TerminalPosition.TOP_LEFT_CORNER;
+        for(int y = 0; y < table.length; y++) {
+            tableCellTopLeft = tableCellTopLeft.withColumn(0);
+            for(int x = 0; x < numberOfColumns; x++) {
+                Component component = table[y][x];
+                if(component != null && !positionMap.containsKey(component)) {
+                    GridLayoutData layoutData = getLayoutData(component);
+                    TerminalSize size = component.getPreferredSize();
+                    TerminalPosition position = tableCellTopLeft;
+
+                    int availableHorizontalSpace = 0;
+                    int availableVerticalSpace = 0;
+                    for (int i = 0; i < layoutData.horizontalSpan; i++) {
+                        availableHorizontalSpace += columnWidths[x + i];
+                    }
+                    for (int i = 0; i < layoutData.verticalSpan; i++) {
+                        availableVerticalSpace += rowHeights[y + i];
+                    }
+
+                    //Make sure to obey the size restrictions
+                    size = size.withColumns(Math.min(size.getColumns(), availableHorizontalSpace));
+                    size = size.withRows(Math.min(size.getRows(), availableVerticalSpace));
+
+                    switch (layoutData.horizontalAlignment) {
+                        case CENTER:
+                            position = position.withRelativeColumn((availableHorizontalSpace - size.getColumns()) / 2);
+                            break;
+                        case END:
+                            position = position.withRelativeColumn(availableHorizontalSpace - size.getColumns());
+                            break;
+                        case FILL:
+                            size = size.withColumns(availableHorizontalSpace);
+                            break;
+                    }
+                    switch (layoutData.verticalAlignment) {
+                        case CENTER:
+                            position = position.withRelativeRow((availableVerticalSpace - size.getRows()) / 2);
+                            break;
+                        case END:
+                            position = position.withRelativeRow(availableVerticalSpace - size.getRows());
+                            break;
+                        case FILL:
+                            size = size.withRows(availableVerticalSpace);
+                            break;
+                    }
+                    sizeMap.put(component, size);
+                    positionMap.put(component, position);
+                }
+
+                tableCellTopLeft = tableCellTopLeft.withRelativeColumn(columnWidths[x]);
+            }
+            tableCellTopLeft = tableCellTopLeft.withRelativeRow(rowHeights[y]);
+        }
+
+        for(Component component: components) {
+            component.setPosition(positionMap.get(component));
+            component.setSize(sizeMap.get(component));
+        }
+    }
+
+    private int[] getPreferredColumnWidths(Component[][] table) {
+        int columnWidths[] = new int[numberOfColumns];
+
+        //Start by letting all span = 1 columns take what they need
+        for(Component[] row: table) {
+            for(int i = 0; i < numberOfColumns; i++) {
+                Component component = row[i];
+                if(component == null) {
+                    columnWidths[i] = 0;
+                    continue;
+                }
+                GridLayoutData layoutData = getLayoutData(component);
+                if (layoutData.horizontalSpan == 1) {
+                    columnWidths[i] = Math.max(columnWidths[i], component.getPreferredSize().getColumns());
+                }
+            }
+        }
+
+        //Next, do span > 1 and enlarge if necessary
+        for(Component[] row: table) {
+            for(int i = 0; i < numberOfColumns; ) {
+                Component component = row[i];
+                if(component == null) {
+                    i++;
+                    continue;
+                }
+                GridLayoutData layoutData = getLayoutData(component);
+                if(layoutData.horizontalSpan > 1) {
+                    int accumWidth = 0;
+                    for(int j = i; j < i + layoutData.horizontalSpan; j++) {
+                        accumWidth += columnWidths[j];
+                    }
+
+                    int preferredWidth = component.getPreferredSize().getColumns();
+                    if(preferredWidth > accumWidth) {
+                        int columnOffset = 0;
+                        do {
+                            columnWidths[i + columnOffset++]++;
+                            accumWidth++;
+                            if(columnOffset == layoutData.horizontalSpan) {
+                                columnOffset = 0;
+                            }
+                        }
+                        while(preferredWidth > accumWidth);
+                    }
+                }
+                i += layoutData.horizontalSpan;
+            }
+        }
+        return columnWidths;
+    }
+
+    private int[] getPreferredRowHeights(Component[][] table) {
+        int numberOfRows = table.length;
+        int rowHeights[] = new int[numberOfRows];
+
+        //Start by letting all span = 1 rows take what they need
+        int rowIndex = 0;
+        for(Component[] row: table) {
+            for(int i = 0; i < numberOfColumns; i++) {
+                Component component = row[i];
+                if(component == null) {
+                    continue;
+                }
+                GridLayoutData layoutData = getLayoutData(component);
+                if(layoutData.verticalSpan == 1) {
+                    rowHeights[rowIndex] = Math.max(rowHeights[rowIndex], component.getPreferredSize().getRows());
+                }
+            }
+            rowIndex++;
+        }
+
+        //Next, do span > 1 and enlarge if necessary
+        for(int x = 0; x < numberOfColumns; x++) {
+            for(int y = 0; y < numberOfRows; ) {
+                Component component = table[y][x];
+                if(component == null) {
+                    y++;
+                    continue;
+                }
+                GridLayoutData layoutData = getLayoutData(component);
+                if(layoutData.verticalSpan > 1) {
+                    int accumHeight = 0;
+                    for(int i = y; i < y + layoutData.verticalSpan; i++) {
+                        accumHeight += rowHeights[i];
+                    }
+
+                    int preferredHeight = component.getPreferredSize().getRows();
+                    if(preferredHeight > accumHeight) {
+                        int rowOffset = 0;
+                        do {
+                            rowHeights[y + rowOffset++]++;
+                            accumHeight++;
+                            if(rowOffset == layoutData.verticalSpan) {
+                                rowOffset = 0;
+                            }
+                        }
+                        while(preferredHeight > accumHeight);
+                    }
+                }
+                y += layoutData.verticalSpan;
+            }
+        }
+        return rowHeights;
+    }
+
+    private Set<Integer> getExpandableColumns(Component[][] table) {
+        Set<Integer> expandableColumns = new TreeSet<Integer>();
+        for(Component[] row: table) {
+            for (int i = 0; i < numberOfColumns; i++) {
+                if(row[i] == null) {
+                    continue;
+                }
+                GridLayoutData layoutData = getLayoutData(row[i]);
+                if(layoutData.grabExtraHorizontalSpace) {
+                    expandableColumns.add(i);
+                }
+            }
+        }
+        return expandableColumns;
+    }
+
+    private Set<Integer> getExpandableRows(Component[][] table) {
+        Set<Integer> expandableRows = new TreeSet<Integer>();
+        for(Component[] row: table) {
+            for (int i = 0; i < numberOfColumns; i++) {
+                if(row[i] == null) {
+                    continue;
+                }
+                GridLayoutData layoutData = getLayoutData(row[i]);
+                if(layoutData.grabExtraVerticalSpace) {
+                    expandableRows.add(i);
+                }
+            }
+        }
+        return expandableRows;
+    }
+
+    private int shrinkWidthToFitArea(TerminalSize area, int[] columnWidths) {
+        int totalWidth = 0;
+        for(int width: columnWidths) {
+            totalWidth += width;
+        }
+        if(totalWidth > area.getColumns()) {
+            int columnOffset = 0;
+            do {
+                if(columnWidths[columnOffset] > 0) {
+                    columnWidths[columnOffset]--;
+                    totalWidth--;
+                }
+                if(++columnOffset == numberOfColumns) {
+                    columnOffset = 0;
+                }
+            }
+            while(totalWidth > area.getColumns());
+        }
+        return totalWidth;
+    }
+
+    private int shrinkHeightToFitArea(TerminalSize area, int[] rowHeights) {
+        int totalHeight = 0;
+        for(int width: rowHeights) {
+            totalHeight += width;
+        }
+        if(totalHeight > area.getRows()) {
+            int rowOffset = 0;
+            do {
+                if(rowHeights[rowOffset] > 0) {
+                    rowHeights[rowOffset]--;
+                    totalHeight--;
+                }
+                if(++rowOffset == rowHeights.length) {
+                    rowOffset = 0;
+                }
+            }
+            while(totalHeight > area.getRows());
+        }
+        return totalHeight;
+    }
+
+    private int grabExtraHorizontalSpace(TerminalSize area, int[] columnWidths, Set<Integer> expandableColumns, int totalWidth) {
+        for(int columnIndex: expandableColumns) {
+            columnWidths[columnIndex]++;
+            totalWidth++;
+            if(area.getColumns() == totalWidth) {
+                break;
+            }
+        }
+        return totalWidth;
+    }
+
+    private int grabExtraVerticalSpace(TerminalSize area, int[] rowHeights, Set<Integer> expandableRows, int totalHeight) {
+        for(int rowIndex: expandableRows) {
+            rowHeights[rowIndex]++;
+            totalHeight++;
+            if(area.getColumns() == totalHeight) {
+                break;
+            }
+        }
+        return totalHeight;
     }
 
     private Component[][] buildTable(List<Component> components) {
@@ -150,7 +439,7 @@ public class GridLayout implements LayoutManager {
         int rowCount = 0;
         int rowsExtent = 1;
         Queue<Component> toBePlaced = new LinkedList<Component>(components);
-        while(!toBePlaced.isEmpty() && rowCount < rowsExtent) {
+        while(!toBePlaced.isEmpty() || rowCount < rowsExtent) {
             //Start new row
             Component[] row = new Component[numberOfColumns];
             int[] hspan = new int[numberOfColumns];
--- src/test/java/com/googlecode/lanterna/gui2/GridLayoutTest.java
+++ src/test/java/com/googlecode/lanterna/gui2/GridLayoutTest.java
@@ -16,28 +16,49 @@ public class GridLayoutTest extends TestBase {
     @Override
     public void init(WindowBasedTextGUI textGUI) {
         final BasicWindow window = new BasicWindow("Grid layout test");
-        Panel contentArea = new Panel();
-        contentArea.setLayoutManager(new GridLayout(4));
-        contentArea.addComponent(new EmptySpace(TextColor.ANSI.BLACK, new TerminalSize(4, 2)));
-        contentArea.addComponent(new EmptySpace(TextColor.ANSI.BLUE, new TerminalSize(4, 2)));
-        contentArea.addComponent(new EmptySpace(TextColor.ANSI.CYAN, new TerminalSize(4, 2)));
-        contentArea.addComponent(new EmptySpace(TextColor.ANSI.GREEN, new TerminalSize(4, 2)));
-        contentArea.addComponent(new EmptySpace(TextColor.ANSI.MAGENTA, new TerminalSize(4, 2))
+
+        Panel leftGridPanel = new Panel();
+        leftGridPanel.setLayoutManager(new GridLayout(4));
+        leftGridPanel.addComponent(new EmptySpace(TextColor.ANSI.BLACK, new TerminalSize(4, 2)));
+        leftGridPanel.addComponent(new EmptySpace(TextColor.ANSI.BLUE, new TerminalSize(4, 2)));
+        leftGridPanel.addComponent(new EmptySpace(TextColor.ANSI.CYAN, new TerminalSize(4, 2)));
+        leftGridPanel.addComponent(new EmptySpace(TextColor.ANSI.GREEN, new TerminalSize(4, 2)));
+
+        leftGridPanel.addComponent(new EmptySpace(TextColor.ANSI.MAGENTA, new TerminalSize(4, 2))
                 .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.BEGINNING, GridLayout.Alignment.CENTER, true, false, 4, 1)));
-        contentArea.addComponent(new EmptySpace(TextColor.ANSI.RED, new TerminalSize(4, 2))
-        .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER, GridLayout.Alignment.CENTER, true, false, 4, 1)));
-        contentArea.addComponent(new EmptySpace(TextColor.ANSI.YELLOW, new TerminalSize(4, 2))
+        leftGridPanel.addComponent(new EmptySpace(TextColor.ANSI.RED, new TerminalSize(4, 2))
+                .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER, GridLayout.Alignment.CENTER, true, false, 4, 1)));
+        leftGridPanel.addComponent(new EmptySpace(TextColor.ANSI.YELLOW, new TerminalSize(4, 2))
                 .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.END, GridLayout.Alignment.CENTER, true, false, 4, 1)));
-        contentArea.addComponent(new EmptySpace(TextColor.ANSI.BLACK, new TerminalSize(4, 2))
+        leftGridPanel.addComponent(new EmptySpace(TextColor.ANSI.BLACK, new TerminalSize(4, 2))
                 .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.FILL, GridLayout.Alignment.CENTER, true, false, 4, 1)));
 
-        contentArea.addComponent(new Button("OK", new Runnable() {
+        Panel rightGridPanel = new Panel();
+        rightGridPanel.setLayoutManager(new GridLayout(5));
+        rightGridPanel.addComponent(new EmptySpace(TextColor.ANSI.BLACK, new TerminalSize(4, 2)));
+        rightGridPanel.addComponent(new EmptySpace(TextColor.ANSI.MAGENTA, new TerminalSize(4, 2))
+                .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER, GridLayout.Alignment.BEGINNING, false, true, 1, 4)));
+        rightGridPanel.addComponent(new EmptySpace(TextColor.ANSI.RED, new TerminalSize(4, 2))
+                .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER, GridLayout.Alignment.CENTER, false, true, 1, 4)));
+        rightGridPanel.addComponent(new EmptySpace(TextColor.ANSI.YELLOW, new TerminalSize(4, 2))
+                .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER, GridLayout.Alignment.END, false, true, 1, 4)));
+        rightGridPanel.addComponent(new EmptySpace(TextColor.ANSI.BLACK, new TerminalSize(4, 2))
+                .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER, GridLayout.Alignment.FILL, false, true, 1, 4)));
+        rightGridPanel.addComponent(new EmptySpace(TextColor.ANSI.BLUE, new TerminalSize(4, 2)));
+        rightGridPanel.addComponent(new EmptySpace(TextColor.ANSI.CYAN, new TerminalSize(4, 2)));
+        rightGridPanel.addComponent(new EmptySpace(TextColor.ANSI.GREEN, new TerminalSize(4, 2)));
+
+        Panel contentPanel = new Panel();
+        contentPanel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
+        contentPanel.addComponent(Panels.horizontal(leftGridPanel, new EmptySpace(TerminalSize.ONE), rightGridPanel));
+        contentPanel.addComponent(new EmptySpace(TerminalSize.ONE));
+        contentPanel.addComponent(new Button("Close", new Runnable() {
             @Override
             public void run() {
                 window.close();
             }
         }));
-        window.setComponent(contentArea);
+        window.setComponent(contentPanel);
         textGUI.addWindow(window);
     }
 }
