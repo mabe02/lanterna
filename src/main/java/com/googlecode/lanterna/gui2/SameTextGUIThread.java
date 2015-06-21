f22c6edb2824ba0e7e9ee3b07dd1f618b84f5342
Martin Berglund <mabe02@gmail.com>
2015-06-21 19:11:10 +0900

    Renaming thread variable

--- src/main/java/com/googlecode/lanterna/gui2/SameTextGUIThread.java
+++ src/main/java/com/googlecode/lanterna/gui2/SameTextGUIThread.java
@@ -1,48 +1,30 @@
 package com.googlecode.lanterna.gui2;
 
 import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by martin on 20/06/15.
  */
 public class SameTextGUIThread extends AbstractTextGUIThread {
 
-    Thread currentThread;
+    private final Thread guiThread;
 
     public SameTextGUIThread(TextGUI textGUI) {
         super(textGUI);
+        guiThread = Thread.currentThread();
     }
 
     @Override
     public Thread getThread() {
-        return currentThread;
+        return guiThread;
     }
 
     @Override
     public void invokeAndWait(Runnable runnable) throws IllegalStateException, InterruptedException {
-        if(currentThread == null || currentThread == Thread.currentThread()) {
+        if(guiThread == null || guiThread == Thread.currentThread()) {
             runnable.run();
         }
         super.invokeAndWait(runnable);
-    }
-
-    @Override
-    public synchronized boolean processEventsAndUpdate() throws IOException {
-        if(currentThread != null && currentThread != Thread.currentThread()) {
-            throw new IllegalStateException(currentThread + " is already processing TextGUI events, " +
-                    Thread.currentThread() + " tried to call processEventsAndUpdate()");
-        }
-        boolean resetCurrentThread = currentThread == null;
-        currentThread = Thread.currentThread();
-        try {
-            return super.processEventsAndUpdate();
-        }
-        finally {
-            if(resetCurrentThread) {
-                currentThread = null;
-            }
-        }
     }
 
     public static class Factory implements TextGUIThreadFactory {
