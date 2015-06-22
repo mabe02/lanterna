dbeb05335e180715581c01058191c645f0ed855a
Martin Berglund <mabe02@gmail.com>
2015-06-22 22:27:47 +0900

    Expanded test

--- src/test/java/com/googlecode/lanterna/gui2/MiscComponentTest.java
+++ src/test/java/com/googlecode/lanterna/gui2/MiscComponentTest.java
@@ -20,7 +20,8 @@ public class MiscComponentTest extends TestBase {
     public void init(WindowBasedTextGUI textGUI) {
         final BasicWindow window = new BasicWindow("Grid layout test");
 
-        Panel leftPanel = new Panel();
+        final Panel leftPanel = new Panel();
+
         Panel checkBoxPanel = new Panel();
         for(int i = 0; i < 4; i++) {
             CheckBox checkBox = new CheckBox("Checkbox #" + (i+1));
@@ -31,8 +32,18 @@ public class MiscComponentTest extends TestBase {
         textBoxPanel.addComponent(Panels.horizontal(new Label("Normal:   "), new TextBox(new TerminalSize(12, 1), "Text")));
         textBoxPanel.addComponent(Panels.horizontal(new Label("Password: "), new TextBox(new TerminalSize(12, 1), "Text").setMask('*')));
 
+        Panel buttonPanel = new Panel();
+        buttonPanel.addComponent(new Button("Enable spacing", new Runnable() {
+            @Override
+            public void run() {
+                LinearLayout layoutManager = (LinearLayout) leftPanel.getLayoutManager();
+                layoutManager.setSpacing(layoutManager.getSpacing() == 0 ? 1 : 0);
+            }
+        }));
+
         leftPanel.addComponent(checkBoxPanel.withBorder(Borders.singleLine("CheckBoxes")));
         leftPanel.addComponent(textBoxPanel.withBorder(Borders.singleLine("TextBoxes")));
+        leftPanel.addComponent(buttonPanel.withBorder(Borders.singleLine("Buttons")));
 
         Panel rightPanel = new Panel();
         textBoxPanel = new Panel();
@@ -41,17 +52,22 @@ public class MiscComponentTest extends TestBase {
         readOnlyTextArea.setText(downloadGPL());
         textBoxPanel.addComponent(readOnlyTextArea);
         rightPanel.addComponent(textBoxPanel.withBorder(Borders.singleLine("Read-only")));
+        rightPanel.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Fill));
 
         Panel contentArea = new Panel();
         contentArea.setLayoutManager(new LinearLayout(Direction.VERTICAL));
         contentArea.addComponent(Panels.horizontal(leftPanel, rightPanel));
-        contentArea.addComponent(new Separator(Direction.HORIZONTAL).setPreferredSize(new TerminalSize(16, 1)));
-        contentArea.addComponent(new Button("OK", new Runnable() {
+        contentArea.addComponent(
+                new Separator(Direction.HORIZONTAL).setLayoutData(
+                        LinearLayout.createLayoutData(LinearLayout.Alignment.Fill)));
+        contentArea.addComponent(
+                new Button("OK", new Runnable() {
             @Override
             public void run() {
                 window.close();
             }
-        }));
+        }).setLayoutData(
+                        LinearLayout.createLayoutData(LinearLayout.Alignment.Center)));
         window.setComponent(contentArea);
         textGUI.addWindow(window);
     }
