1bdb64fa964bd8ecc77da4866591f58e7df1de56
Andreas L <avlplus42@gmail.com>
2015-07-16 17:20:04 +0200

    Alternative solution for redirect with Java 6

--- src/main/java/com/googlecode/lanterna/terminal/ansi/UnixLikeTerminal.java
+++ src/main/java/com/googlecode/lanterna/terminal/ansi/UnixLikeTerminal.java
@@ -78,7 +78,14 @@ public abstract class UnixLikeTerminal extends ANSITerminal {
     protected String exec(String... cmd) throws IOException {
         ProcessBuilder pb = new ProcessBuilder(cmd);
         if (ttyDev != null) {
-            tryRedirectInput(pb);
+            //Here's what we try to do, but that is Java 7+ only:
+            // processBuilder.redirectInput(ProcessBuilder.Redirect.from(ttyDev));
+            //instead, for Java 6, we join the cmd into a scriptlet with redirection
+            //and replace cmd by a call to sh with the scriptlet:
+            StringBuilder sb = new StringBuilder();
+            for (String arg : cmd) { sb.append(arg).append(' '); }
+            sb.append("> ").append(ttyDev);
+            cmd = new String[] { "sh", "-c", sb.toString() };
         }
         Process process = pb.start();
         ByteArrayOutputStream stdoutBuffer = new ByteArrayOutputStream();
@@ -97,22 +104,6 @@ public abstract class UnixLikeTerminal extends ANSITerminal {
         }
         reader.close();
         return builder.toString();
-    }
-
-    //This will only work if running Java 7 och higher
-    private void tryRedirectInput(ProcessBuilder processBuilder) {
-        //Here's what we try to do:
-        //processBuilder.redirectInput(ProcessBuilder.Redirect.from(ttyDev));
-        try {
-            Class redirectClass = Class.forName("java.lang.ProcessBuilder$Redirect");
-            Method fromMethod = redirectClass.getMethod("from", File.class);
-            Object source = fromMethod.invoke(null, ttyDev);
-            Method redirectInputMethod = ProcessBuilder.class.getMethod("redirectInput", source.getClass());
-            redirectInputMethod.invoke(processBuilder, source);
-        }
-        catch(Throwable ignore) {
-            //Didn't work
-        }
     }
 
     @Override
