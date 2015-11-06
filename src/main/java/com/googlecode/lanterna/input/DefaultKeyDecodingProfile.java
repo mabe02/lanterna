1c4922d086a252021e5f7c5c8d88cd8756ed5985
Andreas L <avlplus42@gmail.com>
2015-11-06 18:55:36 +0100

    add EscapeSequenceCharacterPattern to match most special keys.
    Also recogizes shift/alt/control modifiers, if reported by terminal.

--- src/main/java/com/googlecode/lanterna/input/DefaultKeyDecodingProfile.java
+++ src/main/java/com/googlecode/lanterna/input/DefaultKeyDecodingProfile.java
@@ -36,104 +36,17 @@ public class DefaultKeyDecodingProfile implements KeyDecodingProfile {
             = new ArrayList<CharacterPattern>(Arrays.asList(
                             new CharacterPattern[]{
                                 new BasicCharacterPattern(new KeyStroke(KeyType.Escape), ESC_CODE),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowUp), ESC_CODE, '[', 'A'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowUp, false, true), ESC_CODE, ESC_CODE, '[', 'A'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowUp, true, false), ESC_CODE, '[', '1', ';', '5', 'A'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowDown), ESC_CODE, '[', 'B'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowDown, false, true), ESC_CODE, ESC_CODE, '[', 'B'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowDown, true, false), ESC_CODE, '[', '1', ';', '5', 'B'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowRight), ESC_CODE, '[', 'C'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowRight, false, true), ESC_CODE, ESC_CODE, '[', 'C'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowRight, true, false), ESC_CODE, '[', '1', ';', '5', 'C'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowLeft), ESC_CODE, '[', 'D'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowLeft, false, true), ESC_CODE, ESC_CODE, '[', 'D'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ArrowLeft, true, false), ESC_CODE, '[', '1', ';', '5', 'D'),
                                 new BasicCharacterPattern(new KeyStroke(KeyType.Tab), '\t'),
                                 new BasicCharacterPattern(new KeyStroke(KeyType.Enter), '\n'),
                                 new BasicCharacterPattern(new KeyStroke(KeyType.Enter), '\r', '\u0000'), //OS X
-                                new BasicCharacterPattern(new KeyStroke(KeyType.ReverseTab), ESC_CODE, '[', 'Z'),
                                 new BasicCharacterPattern(new KeyStroke(KeyType.Backspace), (char) 0x7f),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.Insert), ESC_CODE, '[', '2', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.Insert, false, true), ESC_CODE, ESC_CODE, '[', '2', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.Delete), ESC_CODE, '[', '3', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.Delete, false, true), ESC_CODE, ESC_CODE, '[', '3', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.Home), ESC_CODE, '[', 'H'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.Home, false, true), ESC_CODE, ESC_CODE, '[', 'H'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.Home), ESC_CODE, 'O', 'H'),  //Gnome terminal
-                                new BasicCharacterPattern(new KeyStroke(KeyType.Home), ESC_CODE, '[', '1', '~'), //Putty
-                                new BasicCharacterPattern(new KeyStroke(KeyType.End), ESC_CODE, '[', 'F'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.End, false, true), ESC_CODE, ESC_CODE, '[', 'F'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.End), ESC_CODE, 'O', 'F'),   //Gnome terminal
-                                new BasicCharacterPattern(new KeyStroke(KeyType.End), ESC_CODE, '[', '4', '~'),  //Putty
-                                new BasicCharacterPattern(new KeyStroke(KeyType.PageUp), ESC_CODE, '[', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.PageUp, false, true), ESC_CODE, ESC_CODE, '[', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.PageDown), ESC_CODE, '[', '6', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.PageDown, false, true), ESC_CODE, ESC_CODE, '[', '6', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F1), ESC_CODE, '[', '1', '1', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F1), ESC_CODE, 'O', 'P'), //Cygwin
                                 new BasicCharacterPattern(new KeyStroke(KeyType.F1), ESC_CODE, '[', '[', 'A'), //Linux
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F2), ESC_CODE, '[', '1', '2', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F2), ESC_CODE, 'O', 'Q'), //Cygwin
                                 new BasicCharacterPattern(new KeyStroke(KeyType.F2), ESC_CODE, '[', '[', 'B'), //Linux
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F3), ESC_CODE, '[', '1', '3', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F3), ESC_CODE, 'O', 'R'), //Cygwin
                                 new BasicCharacterPattern(new KeyStroke(KeyType.F3), ESC_CODE, '[', '[', 'C'), //Linux
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F4), ESC_CODE, '[', '1', '4', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F4), ESC_CODE, 'O', 'S'), //Cygwin
                                 new BasicCharacterPattern(new KeyStroke(KeyType.F4), ESC_CODE, '[', '[', 'D'), //Linux
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F5), ESC_CODE, '[', '1', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F5), ESC_CODE, '[', '1', '6', '~'), 
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F5), ESC_CODE, '[', '3', '2', '~'), //Psion TekLogix
                                 new BasicCharacterPattern(new KeyStroke(KeyType.F5), ESC_CODE, '[', '[', 'E'), //Linux
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F6), ESC_CODE, '[', '1', '7', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F7), ESC_CODE, '[', '1', '8', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F8), ESC_CODE, '[', '1', '9', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F9), ESC_CODE, '[', '2', '0', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F10), ESC_CODE, '[', '2', '1', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F11), ESC_CODE, '[', '2', '3', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F12), ESC_CODE, '[', '2', '4', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F13), ESC_CODE, '[', '2', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F14), ESC_CODE, '[', '2', '6', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F15), ESC_CODE, '[', '2', '8', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F16), ESC_CODE, '[', '2', '9', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F17), ESC_CODE, '[', '3', '1', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F18), ESC_CODE, '[', '3', '2', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F19), ESC_CODE, '[', '3', '3', '~'),
 
-                                //Function keys with alt
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F1, false, true), ESC_CODE, ESC_CODE, '[', '1', '1', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F1, false, true), ESC_CODE, ESC_CODE, 'O', 'P'), //Cygwin
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F2, false, true), ESC_CODE, ESC_CODE, '[', '1', '2', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F2, false, true), ESC_CODE, ESC_CODE, 'O', 'Q'), //Cygwin
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F3, false, true), ESC_CODE, ESC_CODE, '[', '1', '3', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F3, false, true), ESC_CODE, ESC_CODE, 'O', 'R'), //Cygwin
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F4, false, true), ESC_CODE, ESC_CODE, '[', '1', '4', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F4, false, true), ESC_CODE, ESC_CODE, 'O', 'S'), //Cygwin
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F5, false, true), ESC_CODE, ESC_CODE, '[', '1', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F5, false, true), ESC_CODE, ESC_CODE, '[', '1', '6', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F5, false, true), ESC_CODE, ESC_CODE, '[', '3', '2', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F6, false, true), ESC_CODE, ESC_CODE, '[', '1', '7', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F7, false, true), ESC_CODE, ESC_CODE, '[', '1', '8', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F8, false, true), ESC_CODE, ESC_CODE, '[', '1', '9', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F9, false, true), ESC_CODE, ESC_CODE, '[', '2', '0', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F10, false, true), ESC_CODE, ESC_CODE, '[', '2', '1', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F11, false, true), ESC_CODE, ESC_CODE, '[', '2', '3', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F12, false, true), ESC_CODE, ESC_CODE, '[', '2', '4', '~'),
-
-                                //Function keys with ctrl
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F1, true, false), ESC_CODE, '[', '1', ';', '5', 'P'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F2, true, false), ESC_CODE, '[', '1', ';', '5', 'Q'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F3, true, false), ESC_CODE, '[', '1', ';', '5', 'R'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F4, true, false), ESC_CODE, '[', '1', ';', '5', 'S'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F5, true, false), ESC_CODE, '[', '1', '5', ';', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F6, true, false), ESC_CODE, '[', '1', '7', ';', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F7, true, false), ESC_CODE, '[', '1', '8', ';', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F8, true, false), ESC_CODE, '[', '1', '9', ';', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F9, true, false), ESC_CODE, '[', '2', '0', ';', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F10, true, false), ESC_CODE, '[', '2', '1', ';', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F11, true, false), ESC_CODE, '[', '2', '3', ';', '5', '~'),
-                                new BasicCharacterPattern(new KeyStroke(KeyType.F12, true, false), ESC_CODE, '[', '2', '4', ';', '5', '~'),
-
+                                new EscapeSequenceCharacterPattern(),
                                 new NormalCharacterPattern(),
                                 new AltAndCharacterPattern(),
                                 new CtrlAndCharacterPattern(),
