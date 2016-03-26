cadc3e31c0d550b6616f48b981d343d0782f0cc4
Martin Berglund <mabe02@gmail.com>
2016-03-26 23:27:05 +0900

    Updating license header to 2016

--- src/main/java/com/googlecode/lanterna/screen/TabBehaviour.java
+++ src/main/java/com/googlecode/lanterna/screen/TabBehaviour.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * Copyright (C) 2010-2015 Martin
+ * Copyright (C) 2010-2016 Martin
  */
 package com.googlecode.lanterna.screen;
 
