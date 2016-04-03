44031d33a91b5566c7f67573e07a92e96d1b52f1
Martin Berglund <mabe02@gmail.com>
2016-04-03 20:02:09 +0200

    Updating the two native JNA implementations to the new Terminal class hierarchy

--- native-integration/src/main/java/com/googlecode/lanterna/terminal/NativeGNULinuxTerminal.java
+++ native-integration/src/main/java/com/googlecode/lanterna/terminal/NativeGNULinuxTerminal.java
@@ -1,16 +1,27 @@
 package com.googlecode.lanterna.terminal;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.terminal.ansi.UnixLikeTerminal;
 import com.googlecode.lanterna.terminal.ansi.UnixTerminal;
+import com.sun.jna.Native;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
 
+import static com.googlecode.lanterna.terminal.PosixLibC.*;
+import static com.googlecode.lanterna.terminal.PosixLibC.SIGWINCH;
+import static com.googlecode.lanterna.terminal.PosixLibC.STDIN_FILENO;
+
 /**
  * Created by martin on 27/03/16.
  */
-public class NativeGNULinuxTerminal extends UnixTerminal {
+public class NativeGNULinuxTerminal extends UnixLikeTerminal {
+
+    private final PosixLibC libc;
+    private PosixLibC.termios savedTerminalState;
+
     public NativeGNULinuxTerminal() throws IOException {
         this(System.in,
                 System.out,
@@ -24,10 +35,77 @@ public class NativeGNULinuxTerminal extends UnixTerminal {
             Charset terminalCharset,
             CtrlCBehaviour terminalCtrlCBehaviour) throws IOException {
 
-        super(new NativeGNULinuxTerminalDeviceController(),
-                terminalInput,
+        super(terminalInput,
                 terminalOutput,
                 terminalCharset,
                 terminalCtrlCBehaviour);
+
+
+        this.libc = (PosixLibC) Native.loadLibrary("c", PosixLibC.class);
+        this.savedTerminalState = null;
+    }
+
+    public void saveTerminalSettings() throws IOException {
+        savedTerminalState = getTerminalState();
+    }
+
+    public void restoreTerminalSettings() throws IOException {
+        if(savedTerminalState != null) {
+            libc.tcsetattr(STDIN_FILENO, TCSANOW, savedTerminalState);
+        }
+    }
+
+    public void keyEchoEnabled(boolean b) throws IOException {
+        PosixLibC.termios state = getTerminalState();
+        if(b) {
+            state.c_lflag |= ECHO;
+        }
+        else {
+            state.c_lflag &= ~ECHO;
+        }
+        libc.tcsetattr(STDIN_FILENO, TCSANOW, state);
+    }
+
+    public void canonicalMode(boolean b) throws IOException {
+        PosixLibC.termios state = getTerminalState();
+        if(b) {
+            state.c_lflag |= ICANON;
+        }
+        else {
+            state.c_lflag &= ~ICANON;
+        }
+        libc.tcsetattr(STDIN_FILENO, TCSANOW, state);
+    }
+
+    public void keyStrokeSignalsEnabled(boolean b) throws IOException {
+        PosixLibC.termios state = getTerminalState();
+        if(b) {
+            state.c_lflag |= ISIG;
+        }
+        else {
+            state.c_lflag &= ~ISIG;
+        }
+        libc.tcsetattr(STDIN_FILENO, TCSANOW, state);
+    }
+
+    public void registerTerminalResizeListener(final Runnable runnable) throws IOException {
+        libc.signal(SIGWINCH, new sig_t() {
+            public synchronized void invoke(int signal) {
+                runnable.run();
+            }
+        });
+    }
+
+    @Override
+    protected TerminalSize findTerminalSize() throws IOException {
+        PosixLibC.winsize winsize = new winsize();
+        libc.ioctl(PosixLibC.STDOUT_FILENO, PosixLibC.TIOCGWINSZ, winsize);
+        return new TerminalSize(winsize.ws_col, winsize.ws_row);
+    }
+
+    private PosixLibC.termios getTerminalState() {
+        PosixLibC.termios termios = new PosixLibC.termios();
+        libc.tcgetattr(STDIN_FILENO, termios);
+        return termios;
     }
 }
--- native-integration/src/main/java/com/googlecode/lanterna/terminal/WindowsTerminal.java
+++ native-integration/src/main/java/com/googlecode/lanterna/terminal/WindowsTerminal.java
@@ -1,10 +1,12 @@
 package com.googlecode.lanterna.terminal;
 
 import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.terminal.ansi.TerminalDeviceControlStrategy;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.terminal.ansi.UnixLikeTerminal;
+import com.sun.jna.Native;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.win32.W32APIOptions;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,6 +16,12 @@ import java.nio.charset.Charset;
  * Created by Martin on 2016-03-27.
  */
 public class WindowsTerminal extends UnixLikeTerminal {
+
+    private final Wincon windowsConsole;
+    private final WinDef.HANDLE consoleInputHandle;
+    private final WinDef.HANDLE consoleOutputHandle;
+
+    private Integer savedTerminalMode;
 
     public WindowsTerminal() throws IOException {
         this(System.in, System.out, Charset.defaultCharset(), CtrlCBehaviour.CTRL_C_KILLS_APPLICATION);
@@ -25,20 +33,91 @@ public class WindowsTerminal extends UnixLikeTerminal {
             Charset terminalCharset,
             CtrlCBehaviour terminalCtrlCBehaviour) throws IOException {
 
-        super(new WindowsTerminalDeviceController(),
-                terminalInput,
+        super(terminalInput,
                 terminalOutput,
                 terminalCharset,
                 terminalCtrlCBehaviour);
+
+        this.windowsConsole = (Wincon) Native.loadLibrary("kernel32", Wincon.class, W32APIOptions.UNICODE_OPTIONS);
+        this.consoleInputHandle = windowsConsole.GetStdHandle(Wincon.STD_INPUT_HANDLE);
+        this.consoleOutputHandle = windowsConsole.GetStdHandle(Wincon.STD_OUTPUT_HANDLE);
+        this.savedTerminalMode = null;
     }
 
     @Override
-    protected WindowsTerminalDeviceController getDeviceControlStrategy() {
-        return (WindowsTerminalDeviceController)super.getDeviceControlStrategy();
+    public synchronized void saveTerminalSettings() throws IOException {
+        this.savedTerminalMode = getConsoleMode();
     }
 
     @Override
-    public synchronized TerminalPosition getCursorPosition() throws IOException {
-        return getDeviceControlStrategy().getCursorPosition();
+    public synchronized void restoreTerminalSettings() throws IOException {
+        if(savedTerminalMode != null) {
+            windowsConsole.SetConsoleMode(consoleInputHandle, savedTerminalMode);
+        }
+    }
+
+    @Override
+    public synchronized void keyEchoEnabled(boolean enabled) throws IOException {
+        int mode = getConsoleMode();
+        if(enabled) {
+            mode |= Wincon.ENABLE_ECHO_INPUT;
+        }
+        else {
+            mode &= ~Wincon.ENABLE_ECHO_INPUT;
+        }
+        windowsConsole.SetConsoleMode(consoleInputHandle, mode);
+    }
+
+    @Override
+    public synchronized void canonicalMode(boolean enabled) throws IOException {
+        int mode = getConsoleMode();
+        if(enabled) {
+            mode |= Wincon.ENABLE_LINE_INPUT;
+        }
+        else {
+            mode &= ~Wincon.ENABLE_LINE_INPUT;
+        }
+        windowsConsole.SetConsoleMode(consoleInputHandle, mode);
+    }
+
+    @Override
+    public synchronized void keyStrokeSignalsEnabled(boolean enabled) throws IOException {
+        int mode = getConsoleMode();
+        if(enabled) {
+            mode |= Wincon.ENABLE_PROCESSED_INPUT;
+        }
+        else {
+            mode &= ~Wincon.ENABLE_PROCESSED_INPUT;
+        }
+        windowsConsole.SetConsoleMode(consoleInputHandle, mode);
+    }
+
+
+    @Override
+    protected TerminalSize findTerminalSize() throws IOException {
+        WinDef.CONSOLE_SCREEN_BUFFER_INFO screenBufferInfo = new WinDef.CONSOLE_SCREEN_BUFFER_INFO();
+        windowsConsole.GetConsoleScreenBufferInfo(consoleOutputHandle, screenBufferInfo);
+        int columns = screenBufferInfo.srWindow.Right - screenBufferInfo.srWindow.Left + 1;
+        int rows = screenBufferInfo.srWindow.Bottom - screenBufferInfo.srWindow.Top + 1;
+        return new TerminalSize(columns, rows);
+    }
+
+    @Override
+    public void registerTerminalResizeListener(Runnable runnable) throws IOException {
+        // Not implemented yet
+    }
+
+    public synchronized TerminalPosition getCursorPosition() {
+        WinDef.CONSOLE_SCREEN_BUFFER_INFO screenBufferInfo = new WinDef.CONSOLE_SCREEN_BUFFER_INFO();
+        windowsConsole.GetConsoleScreenBufferInfo(consoleOutputHandle, screenBufferInfo);
+        int column = screenBufferInfo.dwCursorPosition.X - screenBufferInfo.srWindow.Left;
+        int row = screenBufferInfo.dwCursorPosition.Y - screenBufferInfo.srWindow.Top;
+        return new TerminalPosition(column, row);
+    }
+
+    private int getConsoleMode() {
+        IntByReference lpMode = new IntByReference();
+        windowsConsole.GetConsoleMode(consoleInputHandle, lpMode);
+        return lpMode.getValue();
     }
 }
