a4e3562326fe024ff57f5c2402003eca84018484
Martin Berglund <mabe02@gmail.com>
2016-05-22 11:30:47 +0900

    If the window has no title, don't draw the title holder

--- src/main/java/com/googlecode/lanterna/gui2/FatWindowDecorationRenderer.java
+++ src/main/java/com/googlecode/lanterna/gui2/FatWindowDecorationRenderer.java
@@ -16,7 +16,10 @@ public class FatWindowDecorationRenderer implements WindowDecorationRenderer {
         if(title == null) {
             title = "";
         }
-        title = " " + title.trim() + " ";
+        boolean hasTitle = !title.trim().isEmpty();
+        if(hasTitle) {
+            title = " " + title.trim() + " ";
+        }
 
         ThemeDefinition themeDefinition = window.getTheme().getDefinition(FatWindowDecorationRenderer.class);
         char horizontalLine = themeDefinition.getCharacter("HORIZONTAL_LINE", Symbols.SINGLE_LINE_HORIZONTAL);
@@ -27,49 +30,91 @@ public class FatWindowDecorationRenderer implements WindowDecorationRenderer {
         char topRightCorner = themeDefinition.getCharacter("TOP_RIGHT_CORNER", Symbols.SINGLE_LINE_TOP_RIGHT_CORNER);
         char leftJunction = themeDefinition.getCharacter("LEFT_JUNCTION", Symbols.SINGLE_LINE_T_RIGHT);
         char rightJunction = themeDefinition.getCharacter("RIGHT_JUNCTION", Symbols.SINGLE_LINE_T_LEFT);
-
         TerminalSize drawableArea = graphics.getSize();
-        graphics.applyThemeStyle(themeDefinition.getPreLight());
-        graphics.drawLine(0, drawableArea.getRows() - 2, 0, 1, verticalLine);
-        graphics.drawLine(1, 0, drawableArea.getColumns() - 2, 0, horizontalLine);
-        graphics.drawLine(1, 2, drawableArea.getColumns() - 2, 2, horizontalLine);
-        graphics.setCharacter(0, 0, topLeftCorner);
-        graphics.setCharacter(0, 2, leftJunction);
-        graphics.setCharacter(0, drawableArea.getRows() - 1, bottomLeftCorner);
 
-        graphics.applyThemeStyle(themeDefinition.getNormal());
-        graphics.drawLine(
-                drawableArea.getColumns() - 1, 1,
-                drawableArea.getColumns() - 1, drawableArea.getRows() - 2,
-                verticalLine);
-        graphics.drawLine(
-                1, drawableArea.getRows() - 1,
-                drawableArea.getColumns() - 2, drawableArea.getRows() - 1,
-                horizontalLine);
+        if(hasTitle) {
+            graphics.applyThemeStyle(themeDefinition.getPreLight());
+            graphics.drawLine(0, drawableArea.getRows() - 2, 0, 1, verticalLine);
+            graphics.drawLine(1, 0, drawableArea.getColumns() - 2, 0, horizontalLine);
+            graphics.drawLine(1, 2, drawableArea.getColumns() - 2, 2, horizontalLine);
+            graphics.setCharacter(0, 0, topLeftCorner);
+            graphics.setCharacter(0, 2, leftJunction);
+            graphics.setCharacter(0, drawableArea.getRows() - 1, bottomLeftCorner);
 
-        graphics.setCharacter(drawableArea.getColumns() - 1, 0, topRightCorner);
-        graphics.setCharacter(drawableArea.getColumns() - 1, 2, rightJunction);
-        graphics.setCharacter(drawableArea.getColumns() - 1, drawableArea.getRows() - 1, bottomRightCorner);
+            graphics.applyThemeStyle(themeDefinition.getNormal());
+            graphics.drawLine(
+                    drawableArea.getColumns() - 1, 1,
+                    drawableArea.getColumns() - 1, drawableArea.getRows() - 2,
+                    verticalLine);
+            graphics.drawLine(
+                    1, drawableArea.getRows() - 1,
+                    drawableArea.getColumns() - 2, drawableArea.getRows() - 1,
+                    horizontalLine);
 
-        graphics.applyThemeStyle(themeDefinition.getActive());
-        graphics.drawLine(1, 1, drawableArea.getColumns() - 2, 1, ' ');
-        graphics.putString(1, 1, TerminalTextUtils.fitString(title, drawableArea.getColumns() - 3));
+            graphics.setCharacter(drawableArea.getColumns() - 1, 0, topRightCorner);
+            graphics.setCharacter(drawableArea.getColumns() - 1, 2, rightJunction);
+            graphics.setCharacter(drawableArea.getColumns() - 1, drawableArea.getRows() - 1, bottomRightCorner);
 
-        return graphics.newTextGraphics(OFFSET, graphics.getSize().withRelativeColumns(-2).withRelativeRows(-4));
+            graphics.applyThemeStyle(themeDefinition.getActive());
+            graphics.drawLine(1, 1, drawableArea.getColumns() - 2, 1, ' ');
+            graphics.putString(1, 1, TerminalTextUtils.fitString(title, drawableArea.getColumns() - 3));
+
+            return graphics.newTextGraphics(OFFSET_WITH_TITLE, graphics.getSize().withRelativeColumns(-2).withRelativeRows(-4));
+        }
+        else {
+            graphics.applyThemeStyle(themeDefinition.getPreLight());
+            graphics.drawLine(0, drawableArea.getRows() - 2, 0, 1, verticalLine);
+            graphics.drawLine(1, 0, drawableArea.getColumns() - 2, 0, horizontalLine);
+            graphics.setCharacter(0, 0, topLeftCorner);
+            graphics.setCharacter(0, drawableArea.getRows() - 1, bottomLeftCorner);
+
+            graphics.applyThemeStyle(themeDefinition.getNormal());
+            graphics.drawLine(
+                    drawableArea.getColumns() - 1, 1,
+                    drawableArea.getColumns() - 1, drawableArea.getRows() - 2,
+                    verticalLine);
+            graphics.drawLine(
+                    1, drawableArea.getRows() - 1,
+                    drawableArea.getColumns() - 2, drawableArea.getRows() - 1,
+                    horizontalLine);
+
+            graphics.setCharacter(drawableArea.getColumns() - 1, 0, topRightCorner);
+            graphics.setCharacter(drawableArea.getColumns() - 1, drawableArea.getRows() - 1, bottomRightCorner);
+
+            return graphics.newTextGraphics(OFFSET_WITHOUT_TITLE, graphics.getSize().withRelativeColumns(-2).withRelativeRows(-2));
+        }
     }
 
     @Override
     public TerminalSize getDecoratedSize(Window window, TerminalSize contentAreaSize) {
-        return contentAreaSize
-                .withRelativeColumns(2)
-                .withRelativeRows(4)
-                .max(new TerminalSize(TerminalTextUtils.getColumnWidth(window.getTitle()) + 4, 1));  //Make sure the title fits!
+        if(hasTitle(window)) {
+            return contentAreaSize
+                    .withRelativeColumns(2)
+                    .withRelativeRows(4)
+                    .max(new TerminalSize(TerminalTextUtils.getColumnWidth(window.getTitle()) + 4, 1));  //Make sure the title fits!
+        }
+        else {
+            return contentAreaSize
+                    .withRelativeColumns(2)
+                    .withRelativeRows(2)
+                    .max(new TerminalSize(3, 1));
+        }
     }
 
-    private static final TerminalPosition OFFSET = new TerminalPosition(1, 3);
+    private static final TerminalPosition OFFSET_WITH_TITLE = new TerminalPosition(1, 3);
+    private static final TerminalPosition OFFSET_WITHOUT_TITLE = new TerminalPosition(1, 1);
 
     @Override
     public TerminalPosition getOffset(Window window) {
-        return OFFSET;
+        if(hasTitle(window)) {
+            return OFFSET_WITH_TITLE;
+        }
+        else {
+            return OFFSET_WITHOUT_TITLE;
+        }
+    }
+
+    private boolean hasTitle(Window window) {
+        return !(window.getTitle() == null || window.getTitle().trim().isEmpty());
     }
 }
