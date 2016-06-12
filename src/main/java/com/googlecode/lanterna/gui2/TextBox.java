d4fd46daa9963b6c3f4deacc30339d28abaf011b
Martin Berglund <mabe02@gmail.com>
2016-06-12 11:26:50 +0900

    Fixing NPE when setting up the textbox

--- src/main/java/com/googlecode/lanterna/gui2/TextBox.java
+++ src/main/java/com/googlecode/lanterna/gui2/TextBox.java
@@ -133,12 +133,16 @@ public class TextBox extends AbstractInteractableComponent<TextBox> {
         this.caretWarp = false;
         this.verticalFocusSwitching = true;
         this.horizontalFocusSwitching = (style == Style.SINGLE_LINE);
+        this.caretPosition = TerminalPosition.TOP_LEFT_CORNER;
         this.maxLineLength = -1;
         this.longestRow = 1;    //To fit the cursor
         this.mask = null;
         this.validationPattern = null;
         setText(initialContent);
+
+        // Re-adjust caret position
         this.caretPosition = TerminalPosition.TOP_LEFT_CORNER.withColumn(getLine(0).length());
+
         if (preferredSize == null) {
             preferredSize = new TerminalSize(Math.max(10, longestRow), lines.size());
         }
