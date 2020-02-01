5fe3495c6f557e96b58b7587d12e3232093f0b96
Martin Berglund <mabe02@gmail.com>
2020-02-01 15:32:15 +0900

    Update to use newer Java 7 API

--- src/main/java/com/googlecode/lanterna/terminal/ansi/UnixLikeTTYTerminal.java
+++ src/main/java/com/googlecode/lanterna/terminal/ansi/UnixLikeTTYTerminal.java
@@ -151,17 +151,10 @@ public abstract class UnixLikeTTYTerminal extends UnixLikeTerminal {
     }
 
     protected String exec(String... cmd) throws IOException {
-        if (ttyDev != null) {
-            //Here's what we try to do, but that is Java 7+ only:
-            // processBuilder.redirectInput(ProcessBuilder.Redirect.from(ttyDev));
-            //instead, for Java 6, we join the cmd into a scriptlet with redirection
-            //and replace cmd by a call to sh with the scriptlet:
-            StringBuilder sb = new StringBuilder();
-            for (String arg : cmd) { sb.append(arg).append(' '); }
-            sb.append("< ").append(ttyDev);
-            cmd = new String[] { "sh", "-c", sb.toString() };
-        }
         ProcessBuilder pb = new ProcessBuilder(cmd);
+        if (ttyDev != null) {
+            pb.redirectInput(ProcessBuilder.Redirect.from(ttyDev));
+        }
         Process process = pb.start();
         ByteArrayOutputStream stdoutBuffer = new ByteArrayOutputStream();
         InputStream stdout = process.getInputStream();
