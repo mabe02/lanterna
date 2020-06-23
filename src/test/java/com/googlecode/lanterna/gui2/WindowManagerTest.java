83af1d60e629c7e59a197a6c2b8f66480627839d
levi yourchuck <levi.yourchuck@gmail.com>
2020-06-23 13:46:53 -0500

    added theme selection window to TestBase, so can be used across various tests

--- src/test/java/com/googlecode/lanterna/gui2/WindowManagerTest.java
+++ src/test/java/com/googlecode/lanterna/gui2/WindowManagerTest.java
@@ -7,6 +7,7 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
+import java.util.*;
 
 public class WindowManagerTest extends TestBase {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -17,6 +18,8 @@ public class WindowManagerTest extends TestBase {
     protected MultiWindowTextGUI createTextGUI(Screen screen) {
         return new MultiWindowTextGUI(new SeparateTextGUIThread.Factory(), screen, new CustomWindowManager());
     }
+
+    Set<String> HACK_what_are_these_orderings = new HashSet<>();
 
     @Override
     public void init(WindowBasedTextGUI textGUI) {
@@ -34,16 +37,48 @@ public class WindowManagerTest extends TestBase {
         textGUI.addWindow(mainWindow);
     }
 
-    private static class CustomWindowManager extends DefaultWindowManager {
+    private class CustomWindowManager extends DefaultWindowManager {
+        // not sure how to determine padding decoration
+        final int PAD = 8;
+        int xOffset;
+        @Override
+        public void prepareWindows(WindowBasedTextGUI textGUI, List<Window> allWindows, TerminalSize screenSize) {
+            trackTheOrderings(allWindows);
+            xOffset = 0;
+            super.prepareWindows(textGUI, allWindows, screenSize);
+        }
+    
         @Override
         protected void prepareWindow(TerminalSize screenSize, Window window) {
             super.prepareWindow(screenSize, window);
 
             window.setDecoratedSize(window.getPreferredSize().withRelative(12, 10));
+            
             window.setPosition(new TerminalPosition(
-                    screenSize.getColumns() - window.getDecoratedSize().getColumns() - 1,
+                    screenSize.getColumns() - window.getDecoratedSize().getColumns() - 1 - xOffset - (2*PAD),
                     screenSize.getRows() - window.getDecoratedSize().getRows() - 1
             ));
+            
+            xOffset += window.getDecoratedSize().getColumns();
         }
     }
+    
+    
+    
+    void trackTheOrderings(List<Window> windows) {
+        String token = "";
+        for (Window w : windows) {
+            final int systemHashCode = System.identityHashCode(w);
+            token += systemHashCode + ", ";
+        }
+        token = "[" + token.substring(0, token.length() -2) + "]";
+        HACK_what_are_these_orderings.add(token);
+        
+        if (HACK_what_are_these_orderings.size() > 1) {
+            // put debugger break point here
+            int hmmm_not_sure_what_is_going_on = 9876;
+        }
+        
+    }
+    
 }
