dd87c9d520a79c54e0904093a1718637d41f3c17
levi yourchuck <levi.yourchuck@gmail.com>
2020-06-23 13:46:53 -0500

    added theme selection window to TestBase, so can be used across various tests

--- src/test/java/com/googlecode/lanterna/gui2/TestBase.java
+++ src/test/java/com/googlecode/lanterna/gui2/TestBase.java
@@ -18,35 +18,43 @@
  */
 package com.googlecode.lanterna.gui2;
 
-import com.googlecode.lanterna.TestTerminalFactory;
-import com.googlecode.lanterna.bundle.LanternaThemes;
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.graphics.*;
+import com.googlecode.lanterna.bundle.*;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
+import java.util.*;
 
 /**
  * Some common code for the GUI tests to get a text system up and running on a separate thread
  * @author Martin
  */
 public abstract class TestBase {
+
+    MultiWindowTextGUI textGui;
+    
+
     void run(String[] args) throws IOException, InterruptedException {
         Screen screen = new TestTerminalFactory(args).createScreen();
         screen.startScreen();
-        MultiWindowTextGUI textGUI = createTextGUI(screen);
-        String theme = extractTheme(args);
-        if(theme != null) {
-            textGUI.setTheme(LanternaThemes.getRegisteredTheme(theme));
-        }
-        textGUI.setBlockingIO(false);
-        textGUI.setEOFWhenNoWindows(true);
+        textGui = createTextGUI(screen);
+        assignTheme(extractTheme(args));
+        textGui.setBlockingIO(false);
+        textGui.setEOFWhenNoWindows(true);
         //noinspection ResultOfMethodCallIgnored
-        textGUI.isEOFWhenNoWindows();   //No meaning, just to silence IntelliJ:s "is never used" alert
+        textGui.isEOFWhenNoWindows();   //No meaning, just to silence IntelliJ:s "is never used" alert
 
         try {
-            init(textGUI);
-            AsynchronousTextGUIThread guiThread = (AsynchronousTextGUIThread)textGUI.getGUIThread();
+            textGui.addWindow(makeThemeChangerWindow());
+         
+            init(textGui);
+            
+            arrangeWindows();
+            
+            AsynchronousTextGUIThread guiThread = (AsynchronousTextGUIThread)textGui.getGUIThread();
             guiThread.start();
-            afterGUIThreadStarted(textGUI);
+            afterGUIThreadStarted(textGui);
             guiThread.waitForStop();
         }
         finally {
@@ -64,11 +72,62 @@ public abstract class TestBase {
     }
 
     protected MultiWindowTextGUI createTextGUI(Screen screen) {
-        return new MultiWindowTextGUI(new SeparateTextGUIThread.Factory(), screen);
+        MultiWindowTextGUI gui = new MultiWindowTextGUI(new SeparateTextGUIThread.Factory(), screen, new DefaultWindowManager());
+        //MultiWindowTextGUI gui = new MultiWindowTextGUI(new SeparateTextGUIThread.Factory(), screen);
+        return gui;
     }
 
-    public abstract void init(WindowBasedTextGUI textGUI);
-    public void afterGUIThreadStarted(WindowBasedTextGUI textGUI) {
+    public abstract void init(WindowBasedTextGUI textGui);
+    public void afterGUIThreadStarted(WindowBasedTextGUI textGui) {
         // By default do nothing
     }
+    
+    
+    public Window makeThemeChangerWindow() {
+		    final Window window = new BasicWindow("Themes");
+        ActionListBox themes = new ActionListBox();
+        themes.setPreferredSize(new TerminalSize(30, 16));
+        themes.addItem( "0, theme: default        ", () -> assignTheme("default"));
+        themes.addItem( "1, theme: defrost        ", () -> assignTheme("defrost"));
+        themes.addItem( "2, theme: bigsnake       ", () -> assignTheme("bigsnake"));
+        themes.addItem( "3, theme: conqueror      ", () -> assignTheme("conqueror"));
+        themes.addItem( "4, theme: businessmachine", () -> assignTheme("businessmachine"));
+        themes.addItem( "5, theme: blaster        ", () -> assignTheme("blaster"));
+        
+        window.setComponent(themes);
+        
+        // 
+        // unsure why, there is still some flicker case if ScrollPanel not quite used preferred size 
+        //ScrollPanel scrollPanel = new ScrollPanel(themes);
+        //scrollPanel.setPreferredSize(new TerminalSize(40, 20));
+        //window.setComponent(scrollPanel);
+        return window;
+    }
+    
+    public void assignTheme(String themeName) {
+        if (themeName == null) {
+            return;
+        }
+        Theme theme = LanternaThemes.getRegisteredTheme(themeName);
+        Collection<Window> windows = textGui.getWindows();
+        if (theme != null && windows != null) {
+            for (Window w : windows) {
+                w.setTheme(theme);
+            }
+            textGui.setTheme(theme);
+        }
+    }
+    
+    public void arrangeWindows() {
+        final int PAD = 8;
+        int x = 1;
+        int y = 1;
+        for (Window w : textGui.getWindows()) {
+            TerminalSize size = w.getPreferredSize();
+            w.setPosition(new TerminalPosition(x, y));
+            w.setHints(Collections.singletonList(Window.Hint.FIXED_POSITION));
+            x += size.getColumns() + PAD;
+        }
+    }
+    
 }
