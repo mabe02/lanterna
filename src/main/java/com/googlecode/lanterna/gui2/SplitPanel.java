d4ee3c8d43f0e4d7c377532cacb422874027dc96
Martin Berglund <mabe02@gmail.com>
2023-07-18 22:09:52 +0900

    PR #542: Fixing up various issues in the code

--- src/main/java/com/googlecode/lanterna/gui2/SplitPanel.java
+++ src/main/java/com/googlecode/lanterna/gui2/SplitPanel.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of lanterna (https://github.com/mabe02/lanterna).
- * 
+ *
  * lanterna is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Copyright (C) 2010-2020 Martin Berglund
  */
 package com.googlecode.lanterna.gui2;
@@ -25,29 +25,29 @@ import com.googlecode.lanterna.input.*;
 import java.util.*;
 
 /**
- * 
  * @author ginkoblongata
  */
 public class SplitPanel extends Panel {
-    
+
     private final Component compA;
     private final ImageComponent thumb;
     private final Component compB;
-    
+
     private boolean isHorizontal;
     private double ratio = 0.5;
-    
+
     public static SplitPanel ofHorizontal(Component left, Component right) {
         SplitPanel split = new SplitPanel(left, right, true);
         return split;
     }
+
     public static SplitPanel ofVertical(Component top, Component bottom) {
         SplitPanel split = new SplitPanel(top, bottom, false);
         return split;
     }
-    
+
     /**
-     * 
+     *
      */
     protected SplitPanel(Component a, Component b, boolean isHorizontal) {
         this.compA = a;
@@ -56,11 +56,12 @@ public class SplitPanel extends Panel {
         thumb = makeThumb();
         setLayoutManager(new ScrollPanelLayoutManager());
         setRatio(10, 10);
-        
+
         addComponent(a);
         addComponent(thumb);
         addComponent(b);
     }
+
     ImageComponent makeThumb() {
         ImageComponent imageComponent = new ImageComponent() {
             TerminalSize aSize;
@@ -68,21 +69,30 @@ public class SplitPanel extends Panel {
             TerminalSize tSize;
             TerminalPosition down = null;
             TerminalPosition drag = null;
+
             @Override
             public Result handleKeyStroke(KeyStroke keyStroke) {
-                if (!(keyStroke instanceof MouseAction)) {
-                    return Result.UNHANDLED;
+                Result result;
+                if (keyStroke instanceof MouseAction) {
+                    result = handleMouseAction((MouseAction) keyStroke);
                 }
-                MouseAction mouse = (MouseAction)keyStroke;
-                if (mouse.isMouseDown()) {
+                // TODO: Implement keyboard based resizing
+                else {
+                    result = super.handleKeyStroke(keyStroke);
+                }
+                return result;
+            }
+
+            private Result handleMouseAction(MouseAction mouseAction) {
+                if (mouseAction.isMouseDown()) {
                     aSize = compA.getSize();
                     bSize = compB.getSize();
                     tSize = thumb.getSize();
-                    down = mouse.getPosition();
+                    down = mouseAction.getPosition();
                 }
-                if (mouse.isMouseDrag()) {
-                    drag = mouse.getPosition();
-                    
+                if (mouseAction.isMouseDrag()) {
+                    drag = mouseAction.getPosition();
+
                     // xxxxxxxxxxxxxxxxxxxxx
                     // this is a hack, should not be needed if the pane drag
                     // only on mouse down'd comp stuff was completely working
@@ -90,7 +100,7 @@ public class SplitPanel extends Panel {
                         down = drag;
                     }
                     // xxxxxxxxxxxxxxxxxxxxx
-                    
+
                     int delta = isHorizontal ? drag.minus(down).getColumn() : drag.minus(down).getRow();
                     // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
                     if (isHorizontal) {
@@ -104,24 +114,25 @@ public class SplitPanel extends Panel {
                     }
                     // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
                 }
-                if (mouse.isMouseUp()) {
+                if (mouseAction.isMouseUp()) {
                     down = null;
                     drag = null;
                 }
                 return Result.HANDLED;
             }
         };
-        imageComponent.setEnabled(false);
-        
         return imageComponent;
     }
-    
+
     class ScrollPanelLayoutManager implements LayoutManager {
-    
+
+        boolean hasChanged;
+
         public ScrollPanelLayoutManager() {
-            
+            hasChanged = true;
         }
-        
+
+
         @Override
         public TerminalSize getPreferredSize(List<Component> components) {
             TerminalSize sizeA = compA.getPreferredSize();
@@ -130,23 +141,21 @@ public class SplitPanel extends Panel {
             TerminalSize sizeB = compB.getPreferredSize();
             int bWidth = sizeB.getColumns();
             int bHeight = sizeB.getRows();
-            
+
             int tWidth = thumb.getPreferredSize().getColumns();
             int tHeight = thumb.getPreferredSize().getRows();
-            
+
             if (isHorizontal) {
-                TerminalSize result = new TerminalSize(aWidth + tWidth + bWidth, Math.max(aHeight, Math.max(tHeight, bHeight)));
-                return result;
+                return new TerminalSize(aWidth + tWidth + bWidth, Math.max(aHeight, Math.max(tHeight, bHeight)));
             } else {
-                TerminalSize result = new TerminalSize(Math.max(aWidth, Math.max(tWidth, bWidth)), aHeight + tHeight + bHeight);
-                return result;
+                return new TerminalSize(Math.max(aWidth, Math.max(tWidth, bWidth)), aHeight + tHeight + bHeight);
             }
         }
-        
+
         @Override
         public void doLayout(TerminalSize area, List<Component> components) {
             TerminalSize size = getSize();
-            
+
             // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
             // TODO: themed
             int length = isHorizontal ? size.getRows() : size.getColumns();
@@ -155,59 +164,82 @@ public class SplitPanel extends Panel {
             Theme theme = getTheme();
             ThemeDefinition themeDefinition = theme.getDefaultDefinition();
             ThemeStyle themeStyle = themeDefinition.getNormal();
-            textImage.setAll(new TextCharacter(isHorizontal ? Symbols.SINGLE_LINE_VERTICAL : Symbols.SINGLE_LINE_HORIZONTAL, themeStyle.getForeground(), themeStyle.getBackground()));
+
+            TextCharacter thumbRenderer = TextCharacter.fromCharacter(
+                    isHorizontal ? Symbols.SINGLE_LINE_VERTICAL : Symbols.SINGLE_LINE_HORIZONTAL,
+                    themeStyle.getForeground(),
+                    themeStyle.getBackground());
+            if (thumb.isFocused()) {
+                thumbRenderer = thumbRenderer.withModifier(SGR.BOLD);
+            }
+
+            textImage.setAll(thumbRenderer);
             thumb.setTextImage(textImage);
             // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-            
+
             int tWidth = thumb.getPreferredSize().getColumns();
             int tHeight = thumb.getPreferredSize().getRows();
-                
+
             int w = size.getColumns();
             int h = size.getRows();
-            
+
             if (isHorizontal) {
                 w -= tWidth;
             } else {
                 h -= tHeight;
             }
-            
+
+            TerminalSize compAPrevSize = compA.getSize();
+            TerminalSize compBPrevSize = compB.getSize();
+            TerminalSize thumbPrevSize = thumb.getSize();
+            TerminalPosition compAPrevPos = compA.getPosition();
+            TerminalPosition compBPrevPos = compB.getPosition();
+            TerminalPosition thumbPrevPos = thumb.getPosition();
+
             if (isHorizontal) {
-                int leftWidth = Math.max(0, (int)(w * ratio));
+                int leftWidth = Math.max(0, (int) (w * ratio));
                 int leftHeight = Math.max(0, Math.min(compA.getPreferredSize().getRows(), h));
-                
+
                 int rightWidth = Math.max(0, w - leftWidth);
                 int rightHeight = Math.max(0, Math.min(compB.getPreferredSize().getRows(), h));
-                
+
                 compA.setSize(new TerminalSize(leftWidth, leftHeight));
                 thumb.setSize(thumb.getPreferredSize());
                 compB.setSize(new TerminalSize(rightWidth, rightHeight));
-                
-                compA.setPosition(new TerminalPosition(0,0));
-                thumb.setPosition(new TerminalPosition(leftWidth, h/2 - tHeight/2));
+
+                compA.setPosition(new TerminalPosition(0, 0));
+                thumb.setPosition(new TerminalPosition(leftWidth, h / 2 - tHeight / 2));
                 compB.setPosition(new TerminalPosition(leftWidth + tWidth, 0));
             } else {
                 int leftWidth = Math.max(0, Math.min(compA.getPreferredSize().getColumns(), w));
-                int leftHeight = Math.max(0, (int)(h * ratio));
-                
+                int leftHeight = Math.max(0, (int) (h * ratio));
+
                 int rightWidth = Math.max(0, Math.min(compB.getPreferredSize().getColumns(), w));
                 int rightHeight = Math.max(0, h - leftHeight);
-                
+
                 compA.setSize(new TerminalSize(leftWidth, leftHeight));
                 thumb.setSize(thumb.getPreferredSize());
                 compB.setSize(new TerminalSize(rightWidth, rightHeight));
-                
-                compA.setPosition(new TerminalPosition(0,0));
-                thumb.setPosition(new TerminalPosition(w/2 - tWidth/2, leftHeight));
+
+                compA.setPosition(new TerminalPosition(0, 0));
+                thumb.setPosition(new TerminalPosition(w / 2 - tWidth / 2, leftHeight));
                 compB.setPosition(new TerminalPosition(0, leftHeight + tHeight));
             }
+
+            hasChanged = !compAPrevPos.equals(compA.getPosition()) ||
+                    !compAPrevSize.equals(compA.getSize()) ||
+                    !compBPrevPos.equals(compB.getPosition()) ||
+                    !compBPrevSize.equals(compB.getSize()) ||
+                    !thumbPrevPos.equals(thumb.getPosition()) ||
+                    !thumbPrevSize.equals(thumb.getSize());
         }
-        
+
         @Override
         public boolean hasChanged() {
-            return true;
+            return hasChanged;
         }
     }
-    
+
     /*
      * Use whatever sizing.
      *
@@ -217,8 +249,10 @@ public class SplitPanel extends Panel {
         if (left == 0 || right == 0) {
             ratio = 0.5;
         }
-        int total = Math.abs(left) + Math.abs(right);
-        ratio = (double)left / (double)total;
+        else {
+            int total = Math.abs(left) + Math.abs(right);
+            ratio = (double) left / (double) total;
+        }
     }
 
     public void setThumbVisible(boolean visible) {
@@ -227,8 +261,13 @@ public class SplitPanel extends Panel {
         if (visible) {
             this.setPreferredSize(null);
         } else {
-            thumb.setPreferredSize(new TerminalSize(0, 0));
+            thumb.setPreferredSize(new TerminalSize(1, 1));
         }
+    }
+
+    @Override
+    public boolean isInvalid() {
+        return super.isInvalid();
     }
 }
 
