681fe5ff37d90139b8c5fce1d14b9a5016896613
Martin Berglund <mabe02@gmail.com>
2024-02-20 22:56:48 +0900

    It's a new year again, time to update copyright

--- src/main/java/com/googlecode/lanterna/gui2/menu/MenuBar.java
+++ src/main/java/com/googlecode/lanterna/gui2/menu/MenuBar.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2010-2020 Martin Berglund
+ * Copyright (C) 2010-2024 Martin Berglund
  * Copyright (C) 2017 Bruno Eberhard
  * Copyright (C) 2017 University of Waikato, Hamilton, NZ
  */
